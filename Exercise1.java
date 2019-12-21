b4616cd8a46f3193571563e2def6868ecae9c580
Nabandon <1839304120@qq.com>
2019-12-21 13:04:35 +0800

    122104

---
+++ Exercise1.java
@@ -0,0 +1,9 @@
+public class Exercise1 {
+    public static void main(String[] args) {
+        int a=10;
+        double b=a;
+        String str="hello";
+        System.out.println(str+b);
+    }
+    
+}
