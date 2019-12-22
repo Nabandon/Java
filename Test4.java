8f97c3a977968c25fd87a35e593a6e4fffbcd391
Nabandon <1839304120@qq.com>
2019-12-22 19:13:27 +0800

    java1

---
+++ Test4.java
@@ -0,0 +1,10 @@
+public class Test4 {
+    public static void main(String[] args) {
+        itn a=10;int b=20;
+        int tmp=a;
+        a=b;
+        b=tmp;
+        System.out.println(a,b)
+    }
+    
+}
