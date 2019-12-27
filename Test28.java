e4e2b6186c3ae86479d1e668f2a0848b3fd159d2
Nabandon <1839304120@qq.com>
2019-12-27 12:51:48 +0800

    19122702

---
+++ Test28.java
@@ -0,0 +1,10 @@
+class Test28{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        print(a,b);
+    }
+    public static void print(int a,int b) {
+        System.out.println("a="+b+" b="+a);
+    }
+}
