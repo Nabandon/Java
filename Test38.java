5cab6c8702b9954a4e8ee2d22586875332122012
Nabandon <1839304120@qq.com>
2019-12-29 19:40:03 +0800

    19122904

---
+++ Test38.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Test38{
+    public static void main(String[] args) {
+        int[] arr=new int[100];
+        int i;
+        for(i=0;i<arr.length;i++){
+            arr[i]=i+1;
+      
+        }System.out.print(Arrays.toString(arr));
+    }
+}
