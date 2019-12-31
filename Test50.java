72168b5237fc52bfc5b43d296d6f0c74685befa7
Nabandon <1839304120@qq.com>
2019-12-31 21:57:37 +0800

    19123104

---
+++ Test50.java
@@ -0,0 +1,11 @@
+class Test50{
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6};
+       String d= a(arr);
+        System.out.println(d);
+    }
+    public static String a(int[] b){
+    String c="qwer";
+    return c;
+    }
+}
