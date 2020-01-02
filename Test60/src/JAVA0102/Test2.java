618d4218e72a5c916a75e11cba7a87dc907c3630
Nabandon <1839304120@qq.com>
2020-01-02 22:40:23 +0800

    20010202

---
+++ Test60/src/JAVA0102/Test2.java
@@ -0,0 +1,21 @@
+package JAVA0102;
+
+public class Test2 {
+    public int age;
+    public String name;
+    public String sex;
+    public void eat(){
+        System.out.println("chifan");
+    }
+    public void sleep(){
+        System.out.println("shuijiao");
+    }
+    public class Main{
+        public  void main(String[] args){}
+        Test2 test2=new Test2();
+        Test2 test3=new Test2();
+        Test2 Test3=new Test2();
+    }
+}
+
+
