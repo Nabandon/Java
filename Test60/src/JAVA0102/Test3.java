0f725de5536158da52cc5e5ffc78305c66c64058
Nabandon <1839304120@qq.com>
2020-01-02 23:02:05 +0800

    20 01 02 03

---
+++ Test60/src/JAVA0102/Test3.java
@@ -0,0 +1,15 @@
+package JAVA0102;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+class Test3 {
+    public String name;
+    public int age;
+}
+class Test0{
+    public static void main(String[] args) {
+        Test3 a=new Test3();
+        System.out.println(a.name);
+        System.out.println(a.age);
+    }
+}
