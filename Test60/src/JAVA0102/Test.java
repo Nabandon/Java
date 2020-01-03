ec58cee17b8850dd4487d0b7c5f6d10b5e932f44
Nabandon <1839304120@qq.com>
2020-01-03 22:23:49 +0800

    CLASS 06

--- Test60/src/JAVA0102/Test.java
+++ Test60/src/JAVA0102/Test.java
@@ -4,13 +4,12 @@ package JAVA0102;
     public int age;
     public String name;
     public String sex;
-    public void eat(){
+        public void eat(){
         System.out.println("吃饭!");
     }
     public void sleep(){
         System.out.println("睡觉!");
     }
-
      public static void main(String[] args) {
          System.out.println(1);
      }
