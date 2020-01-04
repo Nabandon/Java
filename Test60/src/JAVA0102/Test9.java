a1325d6364cb5dd39963131370e56e04200edc88
Nabandon <1839304120@qq.com>
2020-01-04 19:50:20 +0800

    class 07

---
+++ Test60/src/JAVA0102/Test9.java
@@ -0,0 +1,19 @@
+package JAVA0102;
+
+
+
+class P{
+    private String name="zhangsan";
+    private int age=10;
+    public void show() {
+        System.out.println("woshi:" + name + "," + age);
+    }
+}
+public class Test9 {
+    public static void main(String[] args) {
+
+
+        P person = new P();
+        person.show();
+    }
+}
