dc4fa08ec3cd8d82628f70696a12b8e19f099b9e
Nabandon <1839304120@qq.com>
2020-01-12 15:23:55 +0800

    输入一个链表，输出该链表中倒数第k个结点。

--- Test70/src/Java0112/Test71.java
+++ Test70/src/Java0112/Test71.java
@@ -1,7 +1,34 @@
 package Java0112;
-  class  ListNode{
+class ListNode {
+  public int val;
+  public ListNode next;
 
+  public ListNode(int x) {
+    val = x;
   }
+}
 public class Test71 {
 
-}
+    public ListNode FindKthToTail(ListNode head,int k) {
+      if(head==null){
+        return null;
+      }
+      int size=getSize(head);
+      if(k<0||k>size){
+        return null;
+      }
+      int offset=size-k;
+      ListNode cur=head;
+      for(int i=0;i<offset;i++){
+        cur=cur.next;
+      }
+      return cur;
+    }
+    private int getSize(ListNode head){
+      int size=0;
+      for(ListNode cur=head;cur!=null;cur=cur.next){
+        size++;
+      }return size;
+    }
+  }
+
