0034fb36ad639470c54337580b4b9e7ec4f93e05
Nabandon <1839304120@qq.com>
2020-01-14 12:11:08 +0800

    exercise

---
+++ Test70/src/JAVA011302/T1.java
@@ -0,0 +1,25 @@
+package JAVA011302;
+
+public class T1{
+        public static int[] arrymove(int arr[],int k){
+            int temp[]=new int[arr.length];
+            System.arraycopy(arr, 0, temp, 0, arr.length);
+            k=k%arr.length;
+            int t=0;
+            for(int i=0;i<arr.length;i++){
+                t=(i+k)%arr.length;
+                arr[t]=temp[i];
+            }
+            return arr;
+        }
+        public static void main(String[] args) {
+            int arr[]={1,2,3,4,5,6,7};
+            int k=3;
+            int arry[]=arrymove(arr, k);
+            for(int i=0;i<arry.length;i++){
+                System.out.println(arry[i]);
+            }
+        }
+    }
+
+
