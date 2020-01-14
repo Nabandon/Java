5792b8eb3172801d24c09bff6f3ad1f875ef2e01
Nabandon <1839304120@qq.com>
2020-01-14 16:23:57 +0800

    search Insert position

---
+++ Test70/src/Java0112/Insert.java
@@ -0,0 +1,13 @@
+package Java0112;
+
+public class Insert {
+    public int searchInsert(int[] nums, int target) {
+        int i;
+        for( i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
