d81651dc1c95fe14b7685ccd6a40dca02d7ce6ab
Nabandon <1839304120@qq.com>
2020-01-16 13:39:03 +0800

    MergeArrays

---
+++ Test70/src/Java0112/MergeArrays.java
@@ -0,0 +1,14 @@
+package Java0112;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MergeArrays {
+    public void merge(int[] nums1,int m,int[] nums2,int n){
+        int[] new_nums1= Arrays.copyOf(nums1,nums1.length);
+        int m1=0;
+        int n1=0;
+
+    }
+
+}
