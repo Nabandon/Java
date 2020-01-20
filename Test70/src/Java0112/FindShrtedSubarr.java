351034b7a705ce197a318ce5272fe9721a4ad996
Nabandon <1839304120@qq.com>
2020-01-20 23:22:06 +0800

    FindShrtedSubarr.java

--- Test70/src/Java0112/FindShrtedSubarr.java
+++ Test70/src/Java0112/FindShrtedSubarr.java
@@ -6,25 +6,21 @@ public class FindShrtedSubarr {
         System.out.println(findUnsortedSubarray(nums));
     }
     public static int findUnsortedSubarray(int[] nums) {
-            int left=0;
-            int right=0;
-        for (int a = nums.length - 1; a > 0; a--) {
-            for (int b = a - 1; b >= 0; b--) {
-                if (nums[a] < nums[b]) {
-                    right = a + 1;
-                    break;
+        
+                int l = nums.length, r = 0;
+                for (int i = 0; i < nums.length - 1; i++) {
+                    for (int j = i + 1; j < nums.length; j++) {
+                        if (nums[j] < nums[i]) {
+                            r = Math.max(r, j);
+                            l = Math.min(l, i);
+                        }
+                    }
                 }
+                return r - l < 0 ? 0 : r - l + 1;
             }
         }
-        for (int a2 = 0; a2 < nums.length - 1; a2++) {
-            for (int b2 = a2 + 1; b2 < nums.length; b2++) {
-                if (nums[b2] < nums[a2]) {
-                    left = a2;
-                    break;
-                }
-            }
-        }
-        if (right - left < 0) return 0;
-        return right - left;
-    }
-}
+
+
+
+
+
