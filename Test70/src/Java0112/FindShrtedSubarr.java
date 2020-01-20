b30a32ab441fcd5c40f8db2ad7aa5c0125dcef77
Nabandon <1839304120@qq.com>
2020-01-20 22:50:44 +0800

    最短无序连续子数组

---
+++ Test70/src/Java0112/FindShrtedSubarr.java
@@ -0,0 +1,26 @@
+package Java0112;
+
+public class FindShrtedSubarr {
+    public int findUnsortedSubarray(int[] nums) {
+            int left=0;
+            int right=0;
+        for (int a = nums.length - 1; a > 0; a--) {
+            for (int b = a - 1; b >= 0; b--) {
+                if (nums[a] < nums[b]) {
+                    right = a + 1;
+                    break;
+                }
+            }
+        }
+        for (int a2 = 0; a2 < nums.length - 1; a2++) {
+            for (int b2 = a2 + 1; b2 < nums.length; b2++) {
+                if (nums[b2] < nums[a2]) {
+                    left = a2;
+                    break;
+                }
+            }
+        }
+        if (right - left < 0) return 0;
+        return right - right;
+    }
+}
