949a1ebb49afee087c290f65992a48e4a8d8b4a9
Nabandon <1839304120@qq.com>
2020-01-25 21:46:13 +0800

    exercise

---
+++ Test80/src/Test/Test81.java
@@ -0,0 +1,18 @@
+package Test;
+
+class Move{
+    public int remove(int[]nums,int val){
+        int i=0;
+        int n=nums.length;
+        while(i<n){
+            if(nums[i]==val){
+                nums[i]=nums[n-1];
+                n--;
+            }else{
+                i++;
+            }
+        }
+        return i;
+    }
+}
+
