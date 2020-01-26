bff9e686e4d6eaef938ec09e097d879043f03e3e
Nabandon <1839304120@qq.com>
2020-01-26 16:46:15 +0800

    删除链表中的重复节点

---
+++ Test80/src/Test/DeleteDupcation.java
@@ -0,0 +1,27 @@
+package Test;
+
+public class DeleteDupcation {
+    public ListNode deleteDuplication(ListNode pHead){
+        if(pHead==null||pHead.next==null){
+            return pHead;
+        }
+        ListNode head=new ListNode(Integer.MIN_VALUE);
+        head.next=pHead;
+        ListNode pre=head;
+        ListNode cur=head.next;
+        while(cur!=null){
+            if(cur.next!=null&&cur.val==cur.next.val){
+                while(cur.next!=null&&cur.val==cur.next.val){
+                    cur=cur.next;
+                }
+                cur=cur.next;
+                pre.next=cur;
+            }else{
+                pre=cur;
+                cur=cur.next;
+            }
+        }
+        return head.next;
+
+    }
+}
