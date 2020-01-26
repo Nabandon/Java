67605f2c76407a386714cba8700e7208f7b1b197
Nabandon <1839304120@qq.com>
2020-01-26 21:14:11 +0800

    输入两个链表，找出它们的第一个公共结点。

---
+++ Test80/src/Test/PalindromeList.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class PalindromeList {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null||headB==null){
+            return null;
+        }
+        ListNode a=headA;
+        ListNode b=headB;
+        while(a!=b){
+            a= a==null?headB:a.next;
+            b= b==null?headA:b.next;
+        }
+        return a;
+    }
+}
