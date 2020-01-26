54cd289d5b412112aec9d6609917adf4346306be
Nabandon <1839304120@qq.com>
2020-01-26 15:08:55 +0800

    以给定值x为基准将链表分割成两部分，所有小于x的结点排在大于或等于x的结点之前

---
+++ Test80/src/Test/Partition.java
@@ -0,0 +1,30 @@
+package Test;
+
+public class Partition {
+    public ListNode partition(ListNode pHead, int x) {
+        if(pHead==null||pHead.next==null){
+            return pHead;
+        }
+        ListNode cur=pHead;
+        ListNode aHead=new ListNode(-1);
+        ListNode bHead=new ListNode(-1);
+        ListNode a=aHead;
+        ListNode b=bHead;
+        while (cur!=null){
+            if(cur.val<x){
+                a.next=new ListNode(cur.val);
+                a=a.next;
+            }else{
+                b.next=new ListNode(cur.val);
+                b=b.next;
+            }
+            cur=cur.next;
+        }
+        cur=aHead;
+        while(cur.next!=null&&cur.next.val!=-1){
+            cur=cur.next;
+        }
+        cur.next=bHead.next;
+        return  aHead.next;
+    }
+}
