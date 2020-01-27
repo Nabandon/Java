2f878178e225c36e9c34d03306dece63f46fa554
Nabandon <1839304120@qq.com>
2020-01-27 20:50:55 +0800

    给定一个链表，判断链表中是否有环。

---
+++ Test80/src/Test/ListCycle.java
@@ -0,0 +1,19 @@
+package Test;
+
+public class ListCycle {
+    public boolean hasCycle(ListNode head){
+        if(head==null||head.next==null){
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(slow!=fast){
+            if(fast.next==null){
+                return false;
+            }
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return true;
+    }
+}
