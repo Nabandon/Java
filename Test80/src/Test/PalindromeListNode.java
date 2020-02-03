fbf62be225a5f41cd5fe297b9518fe73dab3626c
Nabandon <1839304120@qq.com>
2020-02-03 15:34:03 +0800

    链表回文

--- Test80/src/Test/PalindromeListNode.java
+++ Test80/src/Test/PalindromeListNode.java
@@ -1,26 +1,30 @@
 package Test;
 
 public class PalindromeListNode {
-    private boolean palindrom(ListNode A){
-        ListNode L1=A;
-        ListNode L2=reverse(A);
-        while(L1!=null){
-            if(L1.val!=L2.val){
+   public boolean palindrom(ListNode A) {
+        ListNode b=reverseList(A);
+        while(A!=null){
+            if(A.val!=b.val){
                 return false;
             }
-            L1=L1.next;
-            L2=L2.next;
-        }return true;
-    }
-    private ListNode reverse(ListNode head){
-        ListNode pre=null;
-        ListNode cur=head;
-        while(cur!=null){
-            ListNode temp=cur.next;
-            cur.next=pre;
-            pre=cur;
-            cur=temp;
+            A=A.next;
+            b=b.next;
         }
-        return pre;
+        return true;
+    }
+    private ListNode reverseList(ListNode head){
+       if(head==null){
+           return null;
+       }
+       ListNode nHead=new ListNode(head.val);
+       nHead=null;
+       ListNode cur=head;
+       while (cur!=null){
+           ListNode temp=cur.next;
+           cur.next=nHead;
+           nHead=cur;
+           cur=temp;
+       }
+       return nHead;
     }
 }
