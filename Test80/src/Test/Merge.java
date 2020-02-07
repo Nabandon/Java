c6150f5cec51be49f8056881fa14a1bc16b9c6d0
Nabandon <1839304120@qq.com>
2020-02-07 16:43:20 +0800

    merge1

---
+++ Test80/src/Test/Merge.java
@@ -0,0 +1,8 @@
+package Test;
+import java.util.Arrays;
+public class Merge {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2,0,nums1,m,n);
+        Arrays.sort(nums1);
+    }
+}
