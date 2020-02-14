80509de1f9ae543f8a972f459d60a8781cd6c326
Nabandon <1839304120@qq.com>
2020-02-14 15:57:06 +0800

    exercise

---
+++ Test80/src/Test2/MN.java
@@ -0,0 +1,13 @@
+package Test2;
+
+public class MN {
+    public ListNode m(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
