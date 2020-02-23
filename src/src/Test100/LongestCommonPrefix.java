afd7aadd2478b1b77a3fef22a3be4f8a264b9e1c
Nabandon <1839304120@qq.com>
2020-02-23 11:20:40 +0800

    最长公共前缀

---
+++ src/src/Test100/LongestCommonPrefix.java
@@ -0,0 +1,16 @@
+package Test100;
+
+public class LongestCommonPrefix {
+    public String longest(String[] strs){
+        if(strs.length==0||strs==null) return "";
+        for(int i=0;i<strs[0].length();i++){
+            char c=strs[0].charAt(i);
+            for(int j=1;j<strs.length;j++){
+                if(i==strs[j].length()|| strs[j].charAt(i)!=c){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
