349d7fa493ad0e508f55be395426405208145749
Nabandon <1839304120@qq.com>
2020-02-27 16:49:47 +0800

    链表

--- src/src/Test100/Merge.java
+++ src/src/Test100/Merge.java
@@ -1,5 +1,23 @@
 package Test100;
 
 public class Merge {
-    
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        ListNode node =new ListNode(0);
+        ListNode pre=node;
+        while(l1!=null&&l2!=null){
+            if(l1.val<l2.val){
+                pre.next=l1;
+                pre=pre.next;
+                l1=l1.next;
+            }else {
+                pre.next=l2;
+                pre=pre.next;
+                l2=l2.next;
+            }
+        }
+        pre.next= l1==null?l2:l1;
+        return node.next;
+    }
 }
