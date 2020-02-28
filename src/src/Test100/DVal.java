a00ca0c477a837b7a74db2032f33b5d9495a6b26
Nabandon <1839304120@qq.com>
2020-02-28 21:13:42 +0800

    exercise

--- src/src/Test100/DVal.java
+++ src/src/Test100/DVal.java
@@ -16,4 +16,25 @@ public class DVal {
         }
         return node.next;
     }
+    public ListNode cf(ListNode head){
+        if(head==null||head.next==null) return head;
+        ListNode node=new ListNode( 0);
+        node.next=head;
+        ListNode pre=node;
+        ListNode cur=head;
+        while (cur!=null) {
+            if (cur.next != null && cur.val == cur.next.val) {
+                while (cur.next != null && cur.val == cur.next.val) {
+                    cur = cur.next;
+                }
+                cur = cur.next;
+                pre.next=cur;
+            }else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+        return node.next;
+    }
+
 }
