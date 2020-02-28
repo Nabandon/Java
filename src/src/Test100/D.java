4dfff73ac71a457e52a642aba6211b873aa4b574
Nabandon <1839304120@qq.com>
2020-02-28 21:11:23 +0800

    Test

--- src/src/Test100/D.java
+++ src/src/Test100/D.java
@@ -53,4 +53,20 @@ public class D {
         pre.next= l1==null?l2:l1;
         return pre;
     }
+    public  ListNode d(ListNode head,int val){
+        ListNode node=new ListNode( 0);
+        node.next=head;
+        ListNode pre=node;
+        ListNode cur=head;
+        while(cur!=null){
+            if(cur.val==val){
+                pre.next=cur.next;
+            }else{
+                pre=cur;
+            }
+            cur=cur.next;
+        }
+        return node.next;
+    }
+
 }
