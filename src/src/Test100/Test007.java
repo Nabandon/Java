03d3c40a20c76bd004b906fd5588ae5eaf13f5aa
Nabandon <1839304120@qq.com>
2020-03-02 17:13:15 +0800

    Test

--- src/src/Test100/Test007.java
+++ src/src/Test100/Test007.java
@@ -2,10 +2,9 @@ package Test100;
 
 public class Test007 {
     public static void main(String[] args) {
-        Cat cat=new Cat("小黑");
-        cat.eat("猫粮");
-        Bird bird=new Bird("圆圆");
-        bird.eat("谷子");
-        bird.fly();
+      Anaimal anaimal1=new Anaimal("yuanyuan");
+      anaimal1.eat("gu zi");
+      Anaimal anaimal2=new Bird("BIANBU");
+      anaimal2.eat("gu zi");
     }
 }
