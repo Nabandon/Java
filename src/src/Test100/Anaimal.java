6f70d87ad90cf7343b613c344851b277b5783627
Nabandon <1839304120@qq.com>
2020-03-02 14:31:06 +0800

    Test

--- src/src/Test100/Anaimal.java
+++ src/src/Test100/Anaimal.java
@@ -1,11 +1,13 @@
 package Test100;
 
 public class Anaimal {
-    public String name;
+    protected String name;
     public Anaimal(String name){
         this.name=name;
     }
-    public void eat(String food){
-        System.out.println(this.name+"正在吃"+food);
+        public void eat(String food){
+            System.out.println("wo shi yizhi xiao dong wu");
+            System.out.println(this.name+"正在吃"+food);
+
     }
 }
