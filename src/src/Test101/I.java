2077dfd41301faeb8cf76936a97dddd90bb4bc72
Nabandon <1839304120@qq.com>
2020-03-04 18:57:00 +0800

    test

--- src/src/Test101/I.java
+++ src/src/Test101/I.java
@@ -2,13 +2,8 @@ package Test101;
 
 public class I {
     public static void main(String[] args) {
-      int x=2147395601;
-        long i= 46339;
-        long m=1;
-        while(m<=x){
-            i++;
-            m=i*i;
-        }
-        System.out.println(i-1);
+        System.out.println();
+
+
     }
 }
