a3a94ef4b6406d340e8918e406d33553b988c7dd
Nabandon <1839304120@qq.com>
2020-03-11 15:56:28 +0800

    123模式

--- src/src/Test222/Solution.java
+++ src/src/Test222/Solution.java
@@ -2,13 +2,22 @@ package Test222;
 
 import java.util.Arrays;
 
-public class Solution {
-    public int minMoves(int[] nums) {
-        int count=0;
-        Arrays.sort(nums);
-        for(int i=nums.length-1;i>=0;i--){
-            count+=nums[i]-nums[0];
-        }
-        return count;
+class Solution{
+    public static void main(String[] args) {
+        
     }
+public boolean find132pattern(int[] nums) {
+        if(nums.length<3) return false;
+        int a=0,b=1,c=2,n=nums.length;
+        for(a=0;a<n-2;a++){
+        for(b=a+1;b<n-1;b++){
+        for(c=b+1;c<n;c++){
+        if(nums[a]<nums[c]&&nums[c]<nums[b]){
+            return true;
+        }
+        }
+        }
+        }
+        return false;
+   }
 }
