f02b67fc8787720d8a1a9274bd6cea5bf49c4640
Nabandon <1839304120@qq.com>
2020-03-12 15:14:36 +0800

    部分排序

--- src/src/Test222/Solution.java
+++ src/src/Test222/Solution.java
@@ -2,24 +2,36 @@ package Test222;
 
 import java.util.Arrays;
 
-class Solution{
+class Solution {
     public static void main(String[] args) {
+        int[] arr={1,2,4,7,10,11,7,12,6,7,16,18,19};
 
+        System.out.println(Arrays.toString(subSort(arr)));
     }
-    public boolean find132pattern(int[] nums) {
-        if(nums.length<3) return false;
-        int i=nums[0];
-        for(int j=1;j<nums.length-1;j++){
-            if(nums[j]>i){
-                for(int k=j+1;k<nums.length;k++){
-                    if(nums[k]>i&&nums[k]<nums[j]){
-                        return true;
-                    }
-                }
-            }else{
-                i=nums[j];
-            }
+
+    public static int[] subSort(int[] array) {
+        if (array.length < 2) return array;
+        int left = 0;
+        int right = array.length-1;
+        while (left < array.length-1 && array[left] <= array[left + 1]) {
+            left++;
         }
-        return false;
-   }
+        while (right > 0 && array[right] >= array[right - 1]) {
+            right--;
+        }
+        if (left >= right) return new int[]{-1, -1};
+        int max = Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int k=left;k<=right;k++){
+            max=Math.max(array[k],max);
+            min=Math.min(array[k],min);
+        }
+        while(left>=0&&array[left]>min){
+            left--;
+        }
+        while(right<array.length && array[right]<max){
+            right++;
+        }
+        return new int[]{left+1,right-1};
+    }
 }
