30d96e1465c00d55b31e1bc30bc65078c41ef604
Nabandon <1839304120@qq.com>
2020-03-12 17:27:02 +0800

    输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。

--- src/src/Test222/Solution.java
+++ src/src/Test222/Solution.java
@@ -4,34 +4,21 @@ import java.util.Arrays;
 
 class Solution {
     public static void main(String[] args) {
-        int[] arr={1,2,4,7,10,11,7,12,6,7,16,18,19};
+        ListNode arr=new ListNode(0);
 
-        System.out.println(Arrays.toString(subSort(arr)));
+
     }
 
-    public static int[] subSort(int[] array) {
-        if (array.length < 2) return array;
-        int left = 0;
-        int right = array.length-1;
-        while (left < array.length-1 && array[left] <= array[left + 1]) {
-            left++;
+    public int[] reversePrint(ListNode head) {
+        int size=0;
+        for(ListNode c=head;c!=null;c=c.next){
+            size++;
         }
-        while (right > 0 && array[right] >= array[right - 1]) {
-            right--;
+        int[] arr=new int[size];
+        for(int i=size-1;i>=0;i--){
+            arr[i]=head.val;
+            head=head.next;
         }
-        if (left >= right) return new int[]{-1, -1};
-        int max = Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        for(int k=left;k<=right;k++){
-            max=Math.max(array[k],max);
-            min=Math.min(array[k],min);
-        }
-        while(left>=0&&array[left]>min){
-            left--;
-        }
-        while(right<array.length && array[right]<max){
-            right++;
-        }
-        return new int[]{left+1,right-1};
+        return arr;
     }
 }
