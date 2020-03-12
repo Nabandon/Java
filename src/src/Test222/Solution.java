d894d6477cbb02c7144e9ff6857c5dd000b488f2
Nabandon <1839304120@qq.com>
2020-03-12 14:38:49 +0800

    132

--- src/src/Test222/Solution.java
+++ src/src/Test222/Solution.java
@@ -4,19 +4,21 @@ import java.util.Arrays;
 
 class Solution{
     public static void main(String[] args) {
-        
+
     }
-public boolean find132pattern(int[] nums) {
+    public boolean find132pattern(int[] nums) {
         if(nums.length<3) return false;
-        int a=0,b=1,c=2,n=nums.length;
-        for(a=0;a<n-2;a++){
-        for(b=a+1;b<n-1;b++){
-        for(c=b+1;c<n;c++){
-        if(nums[a]<nums[c]&&nums[c]<nums[b]){
-            return true;
-        }
-        }
-        }
+        int i=nums[0];
+        for(int j=1;j<nums.length-1;j++){
+            if(nums[j]>i){
+                for(int k=j+1;k<nums.length;k++){
+                    if(nums[k]>i&&nums[k]<nums[j]){
+                        return true;
+                    }
+                }
+            }else{
+                i=nums[j];
+            }
         }
         return false;
    }
