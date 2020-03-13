41c1d4fb28a3c430e2cd087833399fd2cec8911a
Nabandon <1839304120@qq.com>
2020-03-13 11:20:59 +0800

    猜数字游戏

--- src/src/Test222/Solution.java
+++ src/src/Test222/Solution.java
@@ -1,28 +1,33 @@
 package Test222;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 class Solution {
     public static void main(String[] args) {
-        ListNode arr=new ListNode(0);
 
-
-    }
-
-
-        public String defangIPaddr(String address) {
-            StringBuilder s = new StringBuilder();
-            for (int i = 0; i < address.length(); i++) {
-                if (address.charAt(i) == '.') {
-
-                    s.append("[.]");
-                } else {
-                    s.append(address.charAt(i));
-                }
+        Random random = new Random();
+        int guess = random.nextInt(100);
+        System.out.println("请输入您猜的数字:");
+        int count=0;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
+            if (num > guess) {
+                System.out.println("猜大了!");
+                count++;
+            } else if (num < guess) {
+                System.out.println("猜小了!");
+                count++;
+            } else {
+                System.out.println("恭喜您猜对了!!!");
+                System.out.println("您一共猜了"+count+"次");
+                return;
             }
-            return s.toString();
         }
     }
+}
 
 
 
