96a8aa4407f9deee91bbb7cb85c73c328e6f73bf
Nabandon <1839304120@qq.com>
2020-03-15 14:52:11 +0800

    字符串Z字形变换

---
+++ src/src/Test222/StringB.java
@@ -0,0 +1,27 @@
+package Test222;
+
+import java.util.*;
+
+public class StringB {
+    public String convert(String s, int numRows) {
+        if(numRows>=s.length()||numRows==1) return s;
+        List<StringBuilder> rows=new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            rows.add(new StringBuilder());
+        }
+        int j=0,flag=-1;
+        for(char c:s.toCharArray()){
+            rows.get(j).append(c);
+            if(j==0||j==numRows-1) {
+                flag=-flag;
+            }
+            j+=flag;
+        }
+        StringBuilder str=new StringBuilder();
+        for(StringBuilder row:rows){
+            str.append(row);
+        }
+
+        return str.toString();
+    }
+}
