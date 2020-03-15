7257dbb8083a9dbce8fc75b94885b1572484e991
Nabandon <1839304120@qq.com>
2020-03-15 10:00:18 +0800

    string

--- src/src/Test222/Solution.java
+++ src/src/Test222/Solution.java
@@ -1,32 +1,50 @@
 package Test222;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
-class Solution {
-    public static void main(String[] args) {
+class Example {
 
-        
+    public static void main(String args[]) {
+//        Example ex = new Example();
+//        ex.change(ex.str,ex.ch);
+//        System.out.print(ex.str + " and ");
+//        System.out.print(ex.ch);
+//    }
+//    public void change(String str,char ch[ ]){
+//        str = "test ok";
+//        ch[0] = 'g';
+//    }
+//    public int indexOf(String str,int fromIdex){
+//       return str.indexOf("word",fromIdex);
+//    }
+//    public String replace(String regex,String replacement){
+//        String all=regex.replaceAll("w","x");
+//        String first=regex.replaceFirst("w","x");
+//        return "wx";
+//    }
+//    public boolean contain(String s){
+//
+//        return s.contains(".m");
+        // }
+ //        public int compareTo () {
+        System.out.println( "asd".compareTo("qwe"));
     }
-    public static long minimumValueAfterDispel (int[] nums) {
+    public int lengthOfLongestSubstring(String s) {
+        int n=s.length();
+        Set<Character> set=new HashSet<>();
+        int i=0;int j=0;int ans=0;
+        while(i<n && j<n){
+            if(!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans=Math.max(ans,j-i);
+            }else{
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
 
-        int sum=0;
-        Arrays.sort(nums);
-        int n=nums[nums.length/2];
-        for(int i=nums.length/2;i<nums.length;i++){
-            nums[i]-=n;
-        }
-        Arrays.sort(nums);
-        int m=nums[nums.length/2];
-        for(int i=nums.length/2;i<nums.length;i++){
-            nums[i]-=m;
-        }
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
-        }
-        return sum;
-   }
+
 }
 //
 
