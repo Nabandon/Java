ac9fed1d6abf0ad56f5c836078dd0f380c3e4eb4
Nabandon <1839304120@qq.com>
2020-03-16 19:22:51 +0800

    两个列表最小索引和

--- src/src/Test222/StringB.java
+++ src/src/Test222/StringB.java
@@ -3,31 +3,25 @@ package Test222;
 import java.util.*;
 
 public class StringB {
-    public String convert(String s, int numRows) {
-        if(numRows>=s.length()||numRows==1) return s;
-        List<StringBuilder> rows=new ArrayList<>();
-        for(int i=0;i<numRows;i++){
-            rows.add(new StringBuilder());
-        }
-        int j=0,flag=-1;
-        for(char c:s.toCharArray()){
-            rows.get(j).append(c);
-            if(j==0||j==numRows-1) {
-                flag=-flag;
+    public static String[] findRestaurant(String[] list1, String[] list2) {
+        String[] list=new String[1000];
+        int k=0;
+        for(int i=0;i<list1.length;i++){
+            for(int j=0;j<list2.length;j++){
+                if(list1[i].equals(list2[j])){
+                    list[ k++]= list1[i];
+                }
             }
-            j+=flag;
-        }
-        StringBuilder str=new StringBuilder();
-        for(StringBuilder row:rows){
-            str.append(row);
         }
 
-        return str.toString();
+        return Arrays.copyOfRange(list,0,k);
+        
     }
-    public StringBuilder s(){
-        String str="ww";
-        StringBuilder str1=new StringBuilder();
-        return str1.append(str);
+
+    public static void main(String[] args) {
+        String[] list1={"a","b","c"};
+        String[] list2={"a","v","m"};
+        System.out.println(findRestaurant(list1,list2));
     }
 
 }
