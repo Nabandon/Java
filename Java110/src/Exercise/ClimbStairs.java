20e2e9619f88ef2ec796b42eff5a4037504e942b
Nabandon <1839304120@qq.com>
2020-03-20 18:18:58 +0800

    最长回文子序列

--- Java110/src/Exercise/ClimbStairs.java
+++ Java110/src/Exercise/ClimbStairs.java
@@ -1,9 +1,23 @@
 package Exercise;
 
 public class ClimbStairs {
-    public int fib(int N) {
-        if(N==0||N==1) return N;
-        return fib(N-1)+fib(N-2);
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        if(n==0||n==1) return  n;
+        int[][] dp=new int[n][n];
+        for(int i=0;i<n;i++){
+            dp[i][i]=1;
+        }
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<=n-len;i++){
+                int j=i+len-1;
+                dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j]=Math.max(dp[i][j],dp[i+1][j-1]+2);
+                }
+            }
+        }
+        return dp[0][n-1];
     }
 
 }
