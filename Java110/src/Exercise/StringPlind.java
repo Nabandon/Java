b752087257959dc5499a3349ca1042c1655b1ffe
Nabandon <1839304120@qq.com>
2020-03-20 14:19:59 +0800

    convertToBase7

--- Java110/src/Exercise/StringPlind.java
+++ Java110/src/Exercise/StringPlind.java
@@ -1,15 +1,19 @@
 package Exercise;
 
 public class StringPlind {
-    public int longestPalindrome(String s) {
-        int[] arr=new int[123];
-        for(char c:s.toCharArray()){
-            arr[c]++;
+    public static void main(String[] args) {
+        StringBuilder s=new StringBuilder("-100");
+        String m=String.valueOf(-100);
+        System.out.println(String.valueOf(m)+s);
+    }
+    public static String convertToBase7(int num) {
+        if(num<7&&num>-7) return String.valueOf(num);
+        StringBuilder str=new StringBuilder();
+        int n=Math.abs(num);
+        while(n>0){
+            str.append(n%7);
+            n/=7;
         }
-        int count=0;
-        for(int x:arr){
-            count+=x%2;
-        }
-        return count==0?s.length():(s.length()-count+1);
+        return num>0?str.reverse().toString() : str.append("-").reverse().toString();
     }
 }
