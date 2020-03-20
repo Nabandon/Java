3a6721ce37ff880a23ab10cb3604eddd3e645ad1
Nabandon <1839304120@qq.com>
2020-03-20 17:12:37 +0800

    完美数

--- Java110/src/Exercise/StringMatch.java
+++ Java110/src/Exercise/StringMatch.java
@@ -2,30 +2,21 @@ package Exercise;
 
 public class StringMatch {
     public static void main(String[] args) {
-        String a="abc";
-        String b="cabcabca";
-        System.out.println(repeatedStringMatch(a,b));
+        System.out.println(checkPerfectNumber(28));
     }
-    public static int repeatedStringMatch(String A, String B) {
-       for(int i=0;i<A.length();i++){
-           if(A.charAt(i)==B.charAt(0)){
-               int count=1;
-               int j=i;
-               int k=0;
-               while (A.charAt(j)==B.charAt(k)){
-                   j++;
-                   k++;
-                   if(k==B.length()){
-                       return count;
-                   }
-                   if(j==A.length()){
-                       j=0;
-                       count++;
-                   }
-
-               }
-           }
-       }
-       return -1;
+    public static boolean checkPerfectNumber(int num) {
+        if(num==1 || num==2) return false;
+        int a=num/2;
+        int b=1;
+        for(int i=2;i<=a;i++){
+            if(num%i==0){
+                b+=i+num/i;
+                a=num/i;
+            }
+        }
+        if(b==num){
+            return true;
+        }
+        return false;
     }
 }
