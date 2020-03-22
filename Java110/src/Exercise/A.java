8e9814a7b8014eef5e781f10179fd7e764aa5034
Nabandon <1839304120@qq.com>
2020-03-22 22:04:38 +0800

    List

---
+++ Java110/src/Exercise/A.java
@@ -0,0 +1,11 @@
+package Exercise;
+
+public class A<T> {
+    T value;
+    A(T x) {
+        value=x;
+    }
+    T get() {
+        return value;
+    }
+}
---
+++ Java110/src/Exercise/B.java
@@ -0,0 +1,11 @@
+package Exercise;
+
+import static javafx.scene.input.KeyCode.T;
+
+public class B {
+    public static void main(String[] args){
+        A<Integer> i=new A<>(0);
+
+        System.out.println(i);
+    }
+}
