00a0e5dd7c857416e1fab5d9083b57e820bbcaf6
Nabandon <1839304120@qq.com>
2020-03-23 12:57:27 +0800

    最小速度

--- Java110/src/Exercise/ClimbStairs.java
+++ Java110/src/Exercise/ClimbStairs.java
@@ -1,22 +1,27 @@
 package Exercise;
 
 public class ClimbStairs {
-    public int longestPalindromeSubseq(String s) {
-        int n = s.length();
-        if(n==0||n==1) return  n;
-        int[][] dp=new int[n][n];
-        for(int i=0;i<n;i++){
-            dp[i][i]=1;
-        }
-        for(int len=2;len<=n;len++){
-            for(int i=0;i<=n-len;i++){
-                int j=i+len-1;
-                dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
-                if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j]=Math.max(dp[i][j],dp[i+1][j-1]+2);
-                }
+
+        public int minEatingSpeed(int[] piles, int H) {
+            int lo = 1;
+            int hi = 1_000_000_000;
+            while (lo < hi) {
+                int mi = (lo + hi) / 2;
+                if (!possible(piles, H, mi))
+                    lo = mi + 1;
+                else
+                    hi = mi;
             }
+
+            return lo;
         }
-        return dp[0][n-1];
+
+
+        public boolean possible(int[] piles, int H, int K) {
+            int time = 0;
+            for (int p: piles)
+                time += (p-1) / K + 1;
+            return time <= H;
+        }
     }
-}
+
