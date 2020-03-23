11bcd2773c0a1ae8067dc7f8578fb35b52856ab8
Nabandon <1839304120@qq.com>
2020-03-23 17:04:08 +0800

    最短完整词

--- Java110/src/Exercise/Solution.java
+++ Java110/src/Exercise/Solution.java
@@ -3,26 +3,46 @@ package Exercise;
 import java.util.Arrays;
 
 public class Solution {
-    public String compressString(String S) {
-        int[] arr=new int[26];
-        for(int i=0;i<S.length();i++){
-
-            arr[ S.charAt(i)-'a']++;
-        }
-        StringBuilder sb=new StringBuilder();
-
-        for(int j=0;j<26;j++){
-            int sum=0;
-            if(arr[j]!=0){
-                sum+=arr[j];
-
-                sb.append((char)j+'a').append(j);
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        int[] lic = new int[26];
+        for (char c : licensePlate.toCharArray()) {
+            if(c<='z'&&c>='a') {
+                lic[c - 'a']++;
+            }else if(c<='Z'&&c>='A'){
+                lic[c-'A']++;
             }
         }
-        if(sb.toString().length()==S.length()){
-            return S;
-        }else{
-            return sb.toString();
+        int len = Integer.MAX_VALUE;
+        String ans = "";
+        for (String w : words) {
+
+            int[] temp = new int[26];
+            for (char c : w.toCharArray()) {
+                if(c<='z'&&c>='a') {
+                    temp[c - 'a']++;
+                }else if(c<='Z'&&c>='A'){
+                    temp[c-'A']++;
+                }
+            }
+            boolean boo=is(temp,lic);
+            if(boo==true){
+                if(w.length()<len){
+                    len=w.length();
+                    ans=w;
+                }
+            }
         }
+            return ans;
+    }
+
+    private boolean is(int[] a1, int[] a2) {
+        for (int i = 0; i < 26; i++) {
+            if (a1[i] < a2[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
+
