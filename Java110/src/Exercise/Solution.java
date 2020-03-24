e58a715881c90c776cbb0d3db163d25fabdd053a
Nabandon <1839304120@qq.com>
2020-03-24 16:44:14 +0800

    字符集合

--- Java110/src/Exercise/Solution.java
+++ Java110/src/Exercise/Solution.java
@@ -1,15 +1,21 @@
 package Exercise;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
-    public int[] smallestK(int[] arr, int k) {
-
-        Arrays.sort(arr);
-        int[] nums=new int[k];
-        for(int i=0;i<k;i++){
-            nums[i]=arr[i];
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNext()) {
+            String str = scanner.nextLine();
+            StringBuilder strb = new StringBuilder();
+            for (int i = 0; i < str.length(); i++) {
+                char tem = str.charAt(i);
+                if (!strb.toString().contains(tem + "")) {
+                    strb.append(tem);
+                }
+            }
+            System.out.println(strb.toString());
         }
-        return nums;
     }
 }
