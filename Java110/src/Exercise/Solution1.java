c8c6516f870c6b8ab39b1e16b2ac9ccffcda011a
Nabandon <1839304120@qq.com>
2020-03-24 18:01:47 +0800

    空格替换

--- Java110/src/Exercise/Solution1.java
+++ Java110/src/Exercise/Solution1.java
@@ -1,24 +1,16 @@
 package Exercise;
 import java.util.Scanner;
 public class Solution1 {
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            String str=in.nextLine();
-            StringBuilder sb=new StringBuilder();
-            int i=0;
-            int size=str.length();
-            while(i<size){
-                char[] c=new char[1];
-                c[0]=str.charAt(i);
-                String s=new String(c);
-                if(sb.toString().contains(s)){
-                    i++;
-                }else{
-                    sb.append(str.charAt(i));
-                    i++;
-                }
+    public String replaceSpace(StringBuffer str) {
+        StringBuilder strb=new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                strb.append("%20");
+            }else{
+                strb.append(str.charAt(i));
             }
-            System.out.println(sb.toString());
         }
+        return strb.toString();
+    }
     }
 
