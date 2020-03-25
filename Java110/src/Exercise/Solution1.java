2dc3361bf72b585477a1bebc303f0c1dd3f7cb89
Nabandon <1839304120@qq.com>
2020-03-25 11:00:12 +0800

    调整数组中数的顺序，奇数位于前半部分，偶数位于后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。

--- Java110/src/Exercise/Solution1.java
+++ Java110/src/Exercise/Solution1.java
@@ -1,18 +1,18 @@
 package Exercise;
 import java.util.Scanner;
 public class Solution1 {
-    public double Power(double base, int exponent) {
-        if(base==0) return 0;
-        if(exponent==0) return 1;
-        if(exponent<0){
-            exponent*=-1;
-            base=1/base;
+    public void reOrderArray(int [] array) {
+        if(array.length<=1) return;
+        int len=array.length;
+        for(int i=0;i<len;i++){
+            for(int j=len-1;j>i;j--){
+                if(array[j]%2!=0&&array[j-1]%2==0){
+                    int temp=array[j];
+                    array[j]=array[j-1];
+                    array[j-1]=temp;
+                }
+            }
         }
-        double b=base;
-        for(int i=1;i<exponent;i++){
-            base*=b;
-        }
-        return base;
     }
 }
 
