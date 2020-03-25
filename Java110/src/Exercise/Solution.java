158615e932c9da2b90d248f88fb79fd17b31d132
Nabandon <1839304120@qq.com>
2020-03-25 09:39:32 +0800

    跳台阶II

--- Java110/src/Exercise/Solution.java
+++ Java110/src/Exercise/Solution.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    public int minNumberInRotateArray(int [] array) {
-        if(array.length==0) return 0;
-        Arrays.sort(array);
-        return array[0];
+    public int JumpFloorII(int n) {
+        if(n<=0) return 0;
+        if(n==1||n==2) return n;
+        else{
+            return JumpFloorII(n-1)*2;
+        }
     }
 }
