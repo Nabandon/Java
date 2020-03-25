ef5a4bddaaa99b92fb4e17d6c6281fcb81d2d60f
Nabandon <1839304120@qq.com>
2020-03-25 10:13:34 +0800

    二进制1的个数

--- Java110/src/Exercise/Solution.java
+++ Java110/src/Exercise/Solution.java
@@ -6,11 +6,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    public int RectCover(int n) {
-        if(n<=0) return 0;
-        if(n==1||n==2||n==3) return n;
-        else{
-            return RectCover(n-1)+RectCover(n-2);
+    public int NumberOf1(int n) {
+        int count=0;
+        while(n!=0){
+            count++;
+            n=n&(n-1);
         }
+        return count;
     }
 }
