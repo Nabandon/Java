f3b7e19f2d7b764315ce04c79a87d337da158431
Nabandon <1839304120@qq.com>
2020-03-25 08:56:04 +0800

    旋转数组的最小值

--- Java110/src/Exercise/Solution.java
+++ Java110/src/Exercise/Solution.java
@@ -1,21 +1,14 @@
 package Exercise;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    public boolean Find(int target, int [][] array) {
-        int h=0,l=array[0].length-1;
-        while(h<array.length && l>=0){
-            if(target<array[h][l]){
-                l--;
-            }
-            else if(target>array[h][l]){
-                h++;
-            }else{
-                return true;
-            }
-        }
-        return false;
+    public int minNumberInRotateArray(int [] array) {
+        if(array.length==0) return 0;
+        Arrays.sort(array);
+        return array[0];
     }
 }
