dd1e6319b9e0a3a7c3f16f77cca15c9cfdc6ea00
Nabandon <1839304120@qq.com>
2020-03-25 09:16:20 +0800

    JumpFloor

--- Java110/src/Exercise/Solution1.java
+++ Java110/src/Exercise/Solution1.java
@@ -1,11 +1,11 @@
 package Exercise;
 import java.util.Scanner;
 public class Solution1 {
-    public int Fibonacci(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        int q=0,h=1;
-        for(int i=1;i<n;i++){
+    public int JumpFloor(int n) {
+        if(n==0||n==1) return 1;
+        if(n==2||n==3) return n;
+        int q=2,h=3;
+        for(int i=3;i<n;i++){
             int res=q+h;
             q=h;
             h=res;
