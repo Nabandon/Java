87fc5a67797ebc0a7f257242c4a7bb5cbec3adae
Nabandon <1839304120@qq.com>
2020-03-25 10:22:07 +0800

    整数次方

--- Java110/src/Exercise/Solution1.java
+++ Java110/src/Exercise/Solution1.java
@@ -1,16 +1,18 @@
 package Exercise;
 import java.util.Scanner;
 public class Solution1 {
-    public int JumpFloor(int n) {
-        if(n==0||n==1) return 1;
-        if(n==2||n==3) return n;
-        int q=2,h=3;
-        for(int i=3;i<n;i++){
-            int res=q+h;
-            q=h;
-            h=res;
+    public double Power(double base, int exponent) {
+        if(base==0) return 0;
+        if(exponent==0) return 1;
+        if(exponent<0){
+            exponent*=-1;
+            base=1/base;
         }
-        return h;
+        double b=base;
+        for(int i=1;i<exponent;i++){
+            base*=b;
+        }
+        return base;
     }
 }
 
