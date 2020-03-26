1b93a12c65dc9975b2a9ff54f8b053b667ac9afc
Nabandon <1839304120@qq.com>
2020-03-26 12:40:42 +0800

    杨辉三角 Pascal's Triangle

--- Java110/src/Exercise/Solution.java
+++ Java110/src/Exercise/Solution.java
@@ -1,17 +1,27 @@
 package Exercise;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    public int NumberOf1(int n) {
-        int count=0;
-        while(n!=0){
-            count++;
-            n=n&(n-1);
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(numRows<=0) return res;
+        List<Integer> internal=new ArrayList<>();
+        internal.add(1);
+        res.add(internal);
+        for(int i=1;i<numRows;i++){
+            List<Integer> list=new ArrayList<>();
+            list.add(1);
+            for(int j=1;j<i;j++){
+                list.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
+            }
+            list.add(1);
+            res.add(list);
         }
-        return count;
+        return res;
     }
 }
