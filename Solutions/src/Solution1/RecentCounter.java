034cf4a3a6cbb60bf3f8d16a6b3c1940336c3977
Nabandon <1839304120@qq.com>
2020-04-01 10:19:52 +0800

    二叉树层序遍历

--- Solutions/src/Solution1/RecentCounter.java
+++ Solutions/src/Solution1/RecentCounter.java
@@ -1,20 +1,24 @@
 package Solution1;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
 class RecentCounter {
-    Queue<Integer> queue;
-    public RecentCounter() {
-        queue=new LinkedList<>();
-    }
-
-    public int ping(int t) {
-        queue.offer(t);
-        while(queue.peek()<t-3000){
-            queue.poll();
+   public void sequenceTraversal(TreeNode root){
+       Queue<TreeNode> queue=new LinkedList<>();//创建一个队列,用来储存节点;
+        queue.offer(root);//将根节点放入队列中;
+        while(  !queue.isEmpty()){//循环取队首元素;
+            TreeNode treeNode=queue.poll();//取队首元素;
+            System.out.println(treeNode.val);//并打印;
+            if(treeNode.left != null){//该节点的左子树不为空,就放入队列;
+                queue.offer(treeNode.left);
+            }
+            if(treeNode.right != null){//该节点右子树不为空,就放入队列;
+                queue.offer(treeNode.right);
+            }
         }
-        return queue.size();
-    }
+   }
 }
 
