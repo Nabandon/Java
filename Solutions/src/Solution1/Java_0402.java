71bd3190ae19761c2958866b6d824f9226f24c95
Nabandon <1839304120@qq.com>
2020-04-08 14:43:32 +0800

    数组中出现次数超过一半的数字

--- Solutions/src/Solution1/Java_0402.java
+++ Solutions/src/Solution1/Java_0402.java
@@ -3,32 +3,27 @@ package Solution1;
 import Solution2.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 
 public class Java_0402 {
 
-    public boolean VerifySquenceOfBST(int [] sequence) {
-        int len=sequence.length;
-        if(len==0) return false;
-        if(len==1) return true;
-        return helper(sequence,0,len-1);
-    }
-    private boolean helper(int[] arr,int start,int end){
-        if(end<=start) return true;
-        int i=start;
-        for(;i<end;i++){
-            if(arr[i]>arr[end]){
-                break;
+    public int MoreThanHalfNum_Solution(int [] array) {
+        int len=array.length;
+        if(len==0) return 0;
+        if(len==1) return array[0];
+        int n=len/2;
+        Arrays.sort(array);
+        int x=array[n];
+        int count=0;
+        for(int i=0;i<len;i++){
+            if(array[i]==x){
+                count++;
             }
         }
-        for(int j=i;j<end;j++){
-            if(arr[j]<arr[end]){
-                return false;
-            }
-        }
-        return helper(arr,0,i-1)&&helper(arr,i,end-1);
+        return count>n?x:0;
     }
 
 }
