44efcb343219c4209c3f89a11b60bdbc66adbae9
Nabandon <1839304120@qq.com>
2020-04-10 20:27:32 +0800

    TopK (堆

--- Solutions/src/Solution1/Java_0402.java
+++ Solutions/src/Solution1/Java_0402.java
@@ -6,20 +6,48 @@ import java.util.*;
 
 
 public class Java_0402 {
-
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        res.add(new ArrayList<>());
-        int len=nums.length;
-        for(int i=0;i<len;i++){
-            int size=res.size();
-            for(int j=0;j<size;j++){
-                List<Integer> temp=new ArrayList<>(res.get(j));
-                temp.add(nums[i]);
-                res.add(temp);
+    public static void main(String[] args) {
+        int[] a={4,5,1,6,2,7,3,8};
+        System.out.println(GetLeastNumbers_Solution(a,4));
+    }
+    public static ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> res=new ArrayList<>();
+        int len=input.length;
+        if(len==0||len<k||k<=0) return res;
+        creatHeap(input,k);
+        for(int i=k;i<len;i++){
+            if(input[i]<input[0]){
+                input[0]=input[i];
+                shiftDown(input,k,0);
             }
         }
+        for(int i=0;i<k;i++){
+            res.add(input[i]);
+        }
         return res;
+    }
+    private static void creatHeap(int[] arr,int k){
+        for(int i=(k-2)/2;i>=0;i--){
+            shiftDown(arr,k,i);
+        }
+    }
+    private static void shiftDown(int [] arr,int size,int index){
+        int parent=index;
+        int child=2*parent+1;
+        while(child<size){
+            if(child+1<size&&arr[child]<arr[child+1]){
+                child=child+1;
+            }
+            if(arr[child]>arr[parent]){
+                int tem=arr[child];
+                arr[child]=arr[parent];
+                arr[parent]=tem;
+            }else{
+                break;
+            }
+            parent=child;
+            child=2*parent+1;
+        }
     }
 
 }
