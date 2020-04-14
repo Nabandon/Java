35dc29924bbf9f5c01bc2b80407059249f69c6af
Nabandon <1839304120@qq.com>
2020-04-14 19:43:56 +0800

    set

--- Solutions/src/Solution1/Java_0402.java
+++ Solutions/src/Solution1/Java_0402.java
@@ -2,38 +2,42 @@ package Solution1;
 
 import Solution2.TreeNode;
 
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.Scanner;
 
 
 public class Java_0402 {
     public static void main(String[] args) {
-    }
-    class Node {
-        int val;
-        Node next;
-        Node random;
 
-        public Node(int val) {
-            this.val = val;
-            this.next = null;
-            this.random = null;
-        }
     }
-    public Node copyRandomList(Node head) {
-        Node node=head;
-        Map<Node,Node> map=new HashMap<>();
-        while(node!=null){
-            Node clone=new Node(node.val);
-            map.put(node,clone);
-            node=node.next;
+    private int baoS(String s,String b){
+        HashSet<Character> set=new HashSet<>();
+        for(char c:s.toCharArray()){
+            set.add(c);
         }
-        node =head;
-        while(node!=null){
-            map.get(node).next=map.get(node.next);
-            map.get(node).random=map.get(node.random);
-            node=node.next;
+        int count = 0;
+        for(char c:b.toCharArray()){
+            if(set.contains(c)){
+                count++;
+            }
         }
-        return map.get(head);
+        return  count;
     }
+    private int efSerch(int[] arr,int k){
+        int left=0;
+        int right=arr.length;
+        while(left<=right){
+            int mind=left+(right-left)/2;
+            if(arr[mind]<k){
+                left=mind+1;
+            }else if(arr[mind]>k){
+                right=mind-1;
+            }else{
+                return  mind;
+            }
 
+        }
+        return -1;
+    }
 }
