aced30cc31a78582aadc93b321a865f947605523
Nabandon <1839304120@qq.com>
2020-04-15 15:19:58 +0800

    前k个高频单词

--- Solutions/src/Solution1/Java_0402.java
+++ Solutions/src/Solution1/Java_0402.java
@@ -8,53 +8,48 @@ import java.util.Scanner;
 
 
 public class Java_0402 {
-    //坏掉的键;
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        while(scanner.hasNext()){
-            String expected=scanner.next();
-            String actual=scanner.next();
-            expected= expected.toUpperCase();
-            actual=  actual.toUpperCase();
-            HashSet<Character> res=new HashSet<>();
-            for(char c:actual.toCharArray()){
-                res.add(c);
+
+    }
+    public List<String> topKFrequent(String[] words,int k){
+        Map<String,Integer> map=new HashMap<>();
+        for(String s:words){
+            map.put(s,map.getOrDefault(s,0)+1);
+        }
+        List<String> list=new ArrayList<>(map.keySet());
+        Collections.sort(list,(o1,o2)->{
+            int count1=map.get(o1);
+            int count2=map.get(o2);
+            if(count1==count2){
+                return o1.compareTo(o2);
             }
-            for(char c:expected.toCharArray()){
-                if(!res.contains(c)){
-                    System.out.print(c);
-                    res.add(c);
+            return count2-count1;
+        });
+        return  list.subList(0,k);
+    }
+    public String[] findRelativeRanks(int[] nums) {
+        int n=nums.length;
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i],i);
+        }
+        Arrays.sort(nums);
+        String[] str=new String[n];
+        int j=n-1;
+        for(int k=0;k<n;k++){
+            if(j>=0){
+                if(j==n-1){
+                    str[map.get(nums[j])]="Gold Medal";
+                }else if(j==n-2){
+                    str[map.get(nums[j])]="Silver Medal";
+                }else if(j==n-3){
+                    str[map.get(nums[j])]="Bronze Medal";
+                }else{
+                    str[map.get(nums[j])]=String.valueOf(k+1);
                 }
+                j--;
             }
         }
-    }
-    //宝石与石头;
-    private int numJewelsInStones(String s,String b){
-        Set<Character> set=new HashSet<>();
-        for(char c:s.toCharArray()){
-            set.add(c);
-        }
-        int count = 0;
-        for(char c:b.toCharArray()){
-            if(set.contains(c)){
-                count++;
-            }
-        }
-        return  count;
-    }
-    private int efSerch(int[] arr,int k){
-        int left=0;
-        int right=arr.length;
-        while(left<=right){
-            int mind=left+(right-left)/2;
-            if(arr[mind]<k){
-                left=mind+1;
-            }else if(arr[mind]>k){
-                right=mind-1;
-            }else{
-                return  mind;
-            }
-        }
-        return -1;
+        return str;
     }
 }
