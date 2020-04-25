070dd33cded6fa3594020c519945f5f1acd9af5c
Nabandon <1839304120@qq.com>
2020-04-25 13:45:55 +0800

    feibo步数

--- Solutions/src/Solution1/Java.java
+++ Solutions/src/Solution1/Java.java
@@ -1,13 +1,29 @@
 package Solution1;
 
+import java.util.Scanner;
+
 public class Java {
     public static void main(String[] args) {
-        String s="Bob. hIt, baLl";
-        String[] e={"bob", "hit"};
-StringBuilder stringBuilder=new StringBuilder();
-stringBuilder.append(s.charAt(0));
-String b=stringBuilder.toString();
-        System.out.println(b);
-       System.out.println(MostCommonword.mostCommonWord(s,e));
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int n=sc.nextInt();
+            System.out.println(fe(n));
+        }
+    }
+    private static int fe(int n){
+        int cur1=0,cur2=1;
+        int sum=cur1+cur2;
+        int min=Integer.MAX_VALUE;
+        while(sum<=n){
+            if(sum==n){
+                return 0;
+            }
+            cur1=cur2;
+            cur2=sum;
+            sum=cur1+cur2;
+            min=Math.min(min,Math.abs(n-sum));
+        }
+        min=Math.min(min,Math.abs(n-sum));
+        return  min;
     }
 }
