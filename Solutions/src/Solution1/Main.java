ad2fe795770ca4d346c941498794c07357c0add8
Nabandon <1839304120@qq.com>
2020-04-26 21:13:11 +0800

    FirstNotRepeatingChar

--- Solutions/src/Solution1/Main.java
+++ Solutions/src/Solution1/Main.java
@@ -1,42 +1,21 @@
 package Solution1;
 
 import java.util.Scanner;
-class TreeNode{
-    char val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode root;
-    TreeNode(char x){
-        val=x;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(FirstNotRepeatingChar("googgle"));
     }
-}
-public class Main{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        while(sc.hasNext()){
-            String str=sc.nextLine();
-            TreeNode root=preorder(str);
-            midisequence(root);
-            System.out.println();
+    public static int FirstNotRepeatingChar(String str) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c:str.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
-    }
-     static int index=0;
-    private static TreeNode preorder(String line){
-        char ch=line.charAt(index);
-        if(ch=='#'){
-            return null;
+        for(int i=0;i<str.length();i++){
+            if(map.get(str.charAt(i))==1){
+                return i;
+            }
         }
-        TreeNode root=new TreeNode(ch);
-        index++;
-        root.left=preorder(line);
-        index++;
-        root.right=preorder(line);
-        return root;
-    }
-    private static void midisequence(TreeNode root){
-        if(root==null) return;
-        midisequence(root.left);
-        System.out.print(root.val+" ");
-        midisequence(root.right);
+        return -1;
     }
 }
