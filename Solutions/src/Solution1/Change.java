55fca9f58060e3941a4ac2727da6ee8dee152b51
Nabandon <1839304120@qq.com>
2020-04-27 14:50:01 +0800

    搜索旋转排序数组(时间复杂度O(log n))

--- Solutions/src/Solution1/Change.java
+++ Solutions/src/Solution1/Change.java
@@ -1,32 +1,28 @@
 package Solution1;
 
 public class Change {
-    public static String change (String number) {
-        // write code her
-        char[] chars=number.toCharArray();
+    public int search(int[] nums, int target) {
         int left=0;
-        int right=number.length()-1;
-        while(left<right){
-            while(left<right && chars[left]%2!=0){
-                left++;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<nums[right]){
+                if(nums[mid]<target && target<=nums[right]){
+                    left=mid+1;
+                }else {
+                    right=mid-1;
+                }
+            }else {
+                if(nums[mid]>target && nums[left]<=target){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
             }
-            while(left<right && chars[right]%2!=0){
-                right--;
-            }
-            char temp=chars[left];
-            chars[left]=chars[right];
-            chars[right]=temp;
-            left++;
-            right--;
         }
-       StringBuilder str=new StringBuilder();
-        for(char c:chars){
-            str.append(c);
-        }
-        return str.toString();
+        return -1;
     }
 
-    public static void main(String[] args) {
-        System.out.println(change("123456"));
-    }
 }
