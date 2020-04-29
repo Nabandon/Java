b0e62fb06f482071cc1a6cd600621329e609bb3b
Nabandon <1839304120@qq.com>
2020-04-29 12:39:06 +0800

    FindContinuousSequence

--- Solutions/src/Solution1/StringFre.java
+++ Solutions/src/Solution1/StringFre.java
@@ -1,41 +1,81 @@
 package Solution1;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StringFre {
-    public int[] numSmallerByFrequency(String[] queries, String[] words) {
-        int wordsLen=words.length;
-        int[] wordsFre=new int[wordsLen];
-        for(int i=0;i<wordsLen;i++){
-            int count=frequency(words[i]);
-            wordsFre[i]=count;
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer> > a=new ArrayList<>();
+        a=FindContinuousSequence(15);
+        for(int i=0;i<a.size();i++){
+            for(int j=0;j<a.get(i).size();j++){
+                System.out.println(a.get(i).get(j));
+            }
         }
-        int[] result=new int[queries.length];
-        for(int i=0;i<queries.length;i++){
-            int count1=frequency(queries[i]);
-            for(int j=0;j<wordsLen;j++){
-                if(count1<wordsFre[j]){
-                    result[i]++;
+    }
+    public static ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        if(sum<3) {
+            return res;
+        }
+        ArrayList<Integer> list =new ArrayList<>();
+        int i=1;
+        int count=0;
+        while(i<=(sum/2+1)){
+            if((count+i)<sum){
+                list.add(i);
+                count+=i;
+                i++;
+            }else if((count+i)==sum){
+                list.add(i);
+                count+=i;
+                res.add(new ArrayList<>(list));
+                count-=list.get(0);
+                list.remove(0);
+                i++;
+            }else {
+                while((count+i)>sum) {
+                    count -= list.get(0);
+                    list.remove(0);
+                }
+                if((count+i)==sum){
+                    list.add(i);
+                    count+=i;
+                    res.add(new ArrayList<>(list));
+                    count-=list.get(0);
+                    list.remove(0);
+
+                }else {
+                    count+=i;
+                    list.add(i);
+                }
+                i++;
+            }
+        }
+        return  res;
+    }
+    //
+    public ArrayList<ArrayList<Integer> > FindContinuousSequence1(int sum) {
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        if(sum<3) {
+            return res;
+        }
+        for(int i=1;i<=(sum/2+1);i++){
+            ArrayList<Integer> list=new ArrayList<>();
+            int num=0;
+            for(int j=i;j<=(sum/2+1);j++){
+                num+=j;
+                list.add(j);
+                if(num==sum){
+                    res.add(list);
+                    break;
+                }
+                if(num>sum){
+                    break;
                 }
             }
         }
-        return result;
-
-    }
-    private int frequency(String s){
-        Map<Character,Integer> map=new HashMap<>();
-        map.put(s.charAt(0),1);
-        char fina=s.charAt(0);
-        for(int i=1;i<s.length();i++){
-           if( s.charAt(i)<fina){
-               map.remove(fina);
-               map.put(s.charAt(i),1);
-               fina=s.charAt(i);
-           }else if(s.charAt(i)==fina){
-               map.put(s.charAt(i),map.get(s.charAt(i))+1);
-           }
-        }
-       return map.get(fina);
+        return res;
     }
 }
