3a2244792b2a2e9b1e61757b3a5c36e33fd5eebe
Nabandon <1839304120@qq.com>
2020-04-30 15:28:54 +0800

    求1+2+3+...+n，不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。

--- Solutions/src/Solution1/Continous.java
+++ Solutions/src/Solution1/Continous.java
@@ -3,25 +3,9 @@ package Solution1;
 import java.util.Arrays;
 
 public class Continous {
-    public boolean isContinuous(int [] numbers) {
-        if(numbers.length!=5){
-            return false;
-        }
-        Arrays.sort(numbers);
-        int ling=0;
-        int min=14,max=-1;
-        for(int i=0;i<numbers.length;i++){
-            if(numbers[i]!=0){
-                min=Math.min(min,numbers[i]);
-                max=Math.max(max,numbers[i]);
-                if(i<numbers.length-1 && numbers[i]==numbers[i+1]){
-                    return false;
-                }
-            }
-        }
-        if(max-min>4){
-            return false;
-        }
-        return true;
+    public int Sum_Solution(int n) {
+        int res=n;
+        boolean b=(n>0) && ((res+=Sum_Solution(n-1))>0);
+        return res;
     }
 }
