76ac64a9babf426bdd51eb583e279888dac6527d
Nabandon <1839304120@qq.com>
2020-04-30 16:46:31 +0800

    duplicate

--- Solutions/src/Solution1/CountAndSay.java
+++ Solutions/src/Solution1/CountAndSay.java
@@ -1,22 +1,22 @@
 package Solution1;
 
 public class CountAndSay {
-    public String countAndSay(int n) {
-        StringBuilder sb=new StringBuilder();
-        if(n==1) return "1";
-        int pre=0;
-        int cur=1;
-        String str=countAndSay(n-1);
-        for(;cur<str.length();cur++){
-            if(str.charAt(cur)!=str.charAt(pre)){
-                int count=cur-pre;
-                sb.append(count).append(str.charAt(pre));
-                pre=cur;
+    public static void main(String[] args) {
+        int len=0;
+        int[] arr={};
+        int[] d=new int[1];
+        System.out.println(duplicate(arr,len,d));
+    }
+    public static boolean duplicate(int numbers[], int length, int[] duplication) {
+        boolean[] k = new boolean[length];
+        for (int i = 0; i < length; i++) {
+            int m=numbers[i];
+            if (k[m] == true) {
+                duplication[0] =m;
+                return true;
             }
+            k[m] = true;
         }
-        if(pre!=cur){
-            sb.append(cur-pre).append(str.charAt(pre));
-        }
-        return sb.toString();
+        return false;
     }
 }
