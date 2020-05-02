595fc41f37b3f1cd1570e78e93450379ffbeb486
Nabandon <1839304120@qq.com>
2020-05-02 16:03:32 +0800

    TreeNode print

--- Solutions/src/Solution1/Match.java
+++ Solutions/src/Solution1/Match.java
@@ -2,8 +2,13 @@ package Solution1;
 
 
 
+import javafx.scene.control.skin.SliderSkin;
+
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 class TreeNode {
     int val;
@@ -16,32 +21,70 @@ class TreeNode {
     }
 }
 public class Match {
-   public boolean isSymmetrical(TreeNode pRoot)
-    {
-        if(pRoot==null){
-            return true;
+    public ArrayList<ArrayList<Integer>> Print1(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        if(pRoot==null) {
+            return res;
         }
-        if(pRoot.left==null && pRoot.right==null){
-            return true;
+        Stack<TreeNode> s1= new Stack<>();
+        s1.push(pRoot);
+        Stack<TreeNode> s2=new Stack<>();
+        while(!s1.isEmpty() || !s2.isEmpty()){
+            if(!s1.isEmpty()){
+                ArrayList<Integer> list=new ArrayList<>();
+                while(!s1.isEmpty()){
+                    TreeNode t=s1.pop();
+                    if(t.left!=null){
+                        s2.push(t.left);
+                    }
+                    if(t.right!=null){
+                        s2.push(t.right);
+                    }
+                    list.add(t.val);
+                }
+                res.add(list);
+            }else {
+                ArrayList<Integer> list=new ArrayList<>();
+                while(!s2.isEmpty()){
+                    TreeNode t=s2.pop();
+                    if(t.right!=null){
+                        s2.push(t.right);
+                    }
+                    if(t.left!=null){
+                        s2.push(t.left);
+                    }
+                    list.add(t.val);
+                }
+                res.add(list);
+            }
         }
-        if(pRoot.left==null || pRoot.right==null){
-            return false;
-        }
-        if(pRoot.left.val==pRoot.right.val){
-            return help(pRoot.left,pRoot.right);
-        }
-        return false;
+        return res;
     }
-        private boolean help(TreeNode l,TreeNode r){
-       if(l==null && r==null) {
-           return true;
-       }
-       if(l==null || r==null){
-           return false;
-       }
-        if(l.val!=r.val){
-        return false;
+    //
+    ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        if(pRoot==null) {
+            return res;
         }
-        return help(l.right,r.left) && help(l.left,r.right);
-   }
+        Queue<TreeNode> queue=new LinkedList<>();
+        int count=0;
+        queue.offer(pRoot);
+        while(!queue.isEmpty()){
+            count=queue.size();
+            ArrayList<Integer> list=new ArrayList<>();
+            while(count>0){
+                TreeNode t=queue.poll();
+                list.add(t.val);
+                if(t.left!=null){
+                    queue.offer(t.left);
+                }
+                if(t.right!=null){
+                    queue.offer(t.right);
+                }
+                count--;
+            }
+            res.add(list);
+        }
+        return res;
+    }
 }
