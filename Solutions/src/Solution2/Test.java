832025f7856d3ecb3fa3085e3c5e8f675afede9f
Nabandon <1839304120@qq.com>
2020-05-03 19:05:21 +0800

    Test

--- Solutions/src/Solution2/Test.java
+++ Solutions/src/Solution2/Test.java
@@ -1,22 +1,40 @@
 package Solution2;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 
 public class Test {
-    public int cutRope(int target) {
-        if(target<=3){
-            return target-1;
+    public static void main(String[] args){
+        Scanner sca=new Scanner(System.in);
+        int n=sca.nextInt();
+        int m=sca.nextInt();
+        int t1=0,t2=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] a=new int[m+n];
+        int[] b=new int[m];
+        for(int i=0;i<n;i++){
+            t1=sca.nextInt();
+            t2=sca.nextInt();
+            a[i]=t1;
+            map.put(t1,t2);
         }
-        if(target%3==0){
-            return (int)Math.pow(3,target/3);
-        }else if(target%3==1){
-            return 4*(int)Math.pow(3,(target-5)/3);
+        for(int i=0;i<m;i++){
+            t1=sca.nextInt();
+            b[i]=t1;
+            a[n+i]=t1;
+            if(!map.containsKey(t1)){
+                map.put(t1,0);
+            }
         }
-        return 2*(int)Math.pow(3,target/3);
+        Arrays.sort(a);
+        int max=0;
+        for(int i=0;i<m+n;i++){
+            max=Math.max(max,map.get(a[i]));
+            map.put(a[i],max);
+        }
+        for(int i=0;i<m;i++){
+            System.out.println(map.get(b[i]));
+        }
     }
-    
 }
