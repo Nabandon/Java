7273aac170689b0aa61676c70d2d5877b6187c89
Nabandon <1839304120@qq.com>
2020-05-04 16:59:36 +0800

    make

--- Solutions/src/Solution1/CountAndSay.java
+++ Solutions/src/Solution1/CountAndSay.java
@@ -1,22 +1,22 @@
 package Solution1;
 
+import java.util.Scanner;
+
 public class CountAndSay {
-    public static void main(String[] args) {
-        int len=0;
-        int[] arr={};
-        int[] d=new int[1];
-        System.out.println(duplicate(arr,len,d));
-    }
-    public static boolean duplicate(int numbers[], int length, int[] duplication) {
-        boolean[] k = new boolean[length];
-        for (int i = 0; i < length; i++) {
-            int m=numbers[i];
-            if (k[m] == true) {
-                duplication[0] =m;
-                return true;
+    public static void main(String[] args){
+        Scanner sca=new Scanner(System.in);
+        int num=sca.nextInt();
+        for(int i=0;i<num;i++){
+            int len=sca.nextInt();
+            String str=sca.next();
+            int count=0;
+            for(int j=0;j<len;j++){
+                if(str.charAt(j)=='.'){
+                    count++;
+                    j+=2;
+                }
             }
-            k[m] = true;
+            System.out.println(count);
         }
-        return false;
     }
 }
