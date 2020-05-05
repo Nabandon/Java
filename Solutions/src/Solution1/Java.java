fc89d22835d2bbddf1dc2729ee41aeaaf3f2d8dd
Nabandon <1839304120@qq.com>
2020-05-05 17:18:45 +0800

    Test

--- Solutions/src/Solution1/Java.java
+++ Solutions/src/Solution1/Java.java
@@ -3,27 +3,28 @@ package Solution1;
 import java.util.Scanner;
 
 public class Java {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            int n=sc.nextInt();
-            System.out.println(fe(n));
+    public static void main(String[] args){
+        Scanner sca=new Scanner(System.in);
+        int n=sca.nextInt();
+        int k=sca.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sca.nextInt();
         }
-    }
-    private static int fe(int n){
-        int cur1=0,cur2=1;
-        int sum=cur1+cur2;
-        int min=Integer.MAX_VALUE;
-        while(sum<=n){
-            if(sum==n){
-                return 0;
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            if((arr[left]+arr[right])<k){
+                left++;
+            }else if((arr[left]+arr[right])==k){
+                if(left==0 || arr[left]!=arr[left-1] || right==n-1){
+                    System.out.println(arr[left]+" " +arr[right]);
+                }
+                left++;
+                right--;
+            }else{
+                right--;
             }
-            cur1=cur2;
-            cur2=sum;
-            sum=cur1+cur2;
-            min=Math.min(min,Math.abs(n-sum));
         }
-        min=Math.min(min,Math.abs(n-sum));
-        return  min;
     }
 }
