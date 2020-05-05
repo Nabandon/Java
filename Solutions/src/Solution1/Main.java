e36a7e51fb856474806b65bf42203b08d955f507
Nabandon <1839304120@qq.com>
2020-05-05 12:08:50 +0800

    整合数组

--- Solutions/src/Solution1/Main.java
+++ Solutions/src/Solution1/Main.java
@@ -2,17 +2,41 @@ package Solution1;
 
 import java.util.Scanner;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args){
-        for(int i=0;i<3;i++){
-            for(int j=0;j<5;j++){
-                if(i==j || i==4-j){
-                    System.out.print("v");
-                }else{
-                    System.out.print(" ");
+        Scanner sca=new Scanner(System.in);
+        int n=sca.nextInt();
+        if(n==1){
+            System.out.println(1);
+            return ;
+        }
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sca.nextInt();
+        }
+        int count=1;
+
+        for(int j=0;j<n;j++){
+            if(count>=n-j+1){
+                break;
+            }
+            Set<Integer> set=new HashSet<>();
+            int max=arr[j] , min=arr[j];
+            set.add(arr[j]);
+            for(int k=j+1 ;k<n;k++){
+                if(set.contains(arr[k])){
+                    break;
+                }
+                set.add(arr[k]);
+                max=Math.max(max,arr[k]);
+                min=Math.min(min,arr[k]);
+                if(max-min==k-j){
+                    count=Math.max(count,max-min+1);
                 }
             }
-            System.out.println();
         }
+        System.out.println(count);
+
     }
 }
