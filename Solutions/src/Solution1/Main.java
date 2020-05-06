0ba5ed73664da08cd6d61a24322acbf20b0cd2f2
Nabandon <1839304120@qq.com>
2020-05-06 16:36:16 +0800

    数对

--- Solutions/src/Solution1/Main.java
+++ Solutions/src/Solution1/Main.java
@@ -6,37 +6,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args){
         Scanner sca=new Scanner(System.in);
-        int n=sca.nextInt();
-        if(n==1){
-            System.out.println(1);
-            return ;
-        }
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sca.nextInt();
-        }
-        int count=1;
-
-        for(int j=0;j<n;j++){
-            if(count>=n-j+1){
-                break;
-            }
-            Set<Integer> set=new HashSet<>();
-            int max=arr[j] , min=arr[j];
-            set.add(arr[j]);
-            for(int k=j+1 ;k<n;k++){
-                if(set.contains(arr[k])){
-                    break;
-                }
-                set.add(arr[k]);
-                max=Math.max(max,arr[k]);
-                min=Math.min(min,arr[k]);
-                if(max-min==k-j){
-                    count=Math.max(count,max-min+1);
+        long n=sca.nextInt();
+        long k=sca.nextInt();
+        long count=0;
+        if(k==0){
+            count=n*n;
+        }else{
+            for(long i=k+1;i<=n;i++){
+                count+=(n/i)*(i-k);
+                if(n%i>=k){
+                    count+=n%i-k+1;
                 }
             }
         }
         System.out.println(count);
-
     }
 }
