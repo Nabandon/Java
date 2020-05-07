c2d0ee4d09f97b83d849868459106f8c0c3dcdc2
Nabandon <1839304120@qq.com>
2020-05-07 16:42:09 +0800

    矩形重叠

--- Solutions/src/Solution1/Main.java
+++ Solutions/src/Solution1/Main.java
@@ -6,19 +6,38 @@ import java.util.*;
 public class Main {
     public static void main(String[] args){
         Scanner sca=new Scanner(System.in);
-        long n=sca.nextInt();
-        long k=sca.nextInt();
-        long count=0;
-        if(k==0){
-            count=n*n;
-        }else{
-            for(long i=k+1;i<=n;i++){
-                count+=(n/i)*(i-k);
-                if(n%i>=k){
-                    count+=n%i-k+1;
+        int n=sca.nextInt();
+        int[] x1=new int[n];
+        int[] y1=new int[n];
+        int[] x2=new int[n];
+        int[] y2=new int[n];
+        for(int i=0;i<n;i++){
+            x1[i]=sca.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            y1[i]=sca.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            x2[i]=sca.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            y2[i]=sca.nextInt();
+        }
+        int ans = 0;
+        int cnt = 0;
+        for (int x : x1) {
+            for (int y : y1) {
+                for (int i = 0; i < n; i++) {
+                    if (x >= x1[i] && x < x2[i] && y >= y1[i] && y < y2[i]) {
+                        cnt++;
+                    }
                 }
+                if (cnt > ans) {
+                    ans = cnt;
+                }
+                cnt = 0;
             }
         }
-        System.out.println(count);
+        System.out.println(ans);
     }
 }
