f5fdcc40aefe45cf88f1ac081e667d8681e5b1d0
Nabandon <1839304120@qq.com>
2020-05-08 14:20:16 +0800

    Test

--- Solutions/src/Solution1/Main.java
+++ Solutions/src/Solution1/Main.java
@@ -7,37 +7,13 @@ public class Main {
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        int w=s.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=s.nextInt();
+        int m=s.nextInt();
+        int[] arr=new int[n+1];
+        for(int i=0;i<m;i++){
+            int k=s.nextInt();
+            arr[k]++;
         }
-        long sum=0;
-        for(int i=0;i<n;i++){
-            sum+=arr[i];
-        }
-        if(sum<=w){
-            System.out.println((int)Math.pow(2,n));
-            return ;
-        }else{
-            System.out.println(loop(arr,n-1,w));
-        }
-    }
-    private static int loop(int[] arr,int i,int w){
-        if(i==0){
-            if(w>=arr[0]){
-                return 2;
-            }else{
-                return 1;
-            }
-        }
-        if(w<=0){
-            return 1;
-        }
-        if(w-arr[i]>=0){
-            return loop(arr,i-1,w)+loop(arr,i-1,w-arr[i]);
-        }else{
-            return loop(arr,i-1,w);
-        }
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
     }
 }
