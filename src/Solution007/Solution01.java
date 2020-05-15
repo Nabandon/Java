f6bacada76722e7872967a93f1b3de2b78e5e39f
Nabandon <1839304120@qq.com>
2020-05-15 19:13:37 +0800

    reorder-list

--- src/Solution007/Solution01.java
+++ src/Solution007/Solution01.java
@@ -3,23 +3,31 @@ package Solution007;
 import java.util.Stack;
 
 public class Solution01 {
-    public ListNode insertionSortList(ListNode head) {
+    public void reorderList(ListNode head) {
         if(head==null || head.next==null){
-            return head;
+            return;
         }
-        ListNode cur=new ListNode(0);
-        ListNode t=head;
-
-        while(t!=null){
-            ListNode tNext=t.next;
-            ListNode pre=cur;
-            while(pre.next!=null && pre.next.val<t.val){
-                pre=pre.next;
-            }
-            t.next=pre.next;
-            pre.next=t;
-            t=tNext;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        return  cur.next;
+        ListNode end=null;
+        while(slow!=null){
+            ListNode t=slow.next;
+            slow.next=end;
+            end=slow;
+            slow=t;
+        }
+        ListNode first=head;
+        while(end!=null && first!=null){
+            ListNode q=first.next;
+            ListNode h=end.next;
+            first.next=end;
+            first=q;
+            end.next=q;
+            end=h;
+        }
     }
 }
