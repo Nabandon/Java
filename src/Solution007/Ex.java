9d9f3f41e8fd04587011cc1fca4542b8e1e6dde1
Nabandon <1839304120@qq.com>
2020-05-16 20:18:02 +0800

    copy-list-with-random-pointer

--- src/Solution007/Ex.java
+++ src/Solution007/Ex.java
@@ -1,68 +1,28 @@
 package Solution007;
 
-class ListNode {
-     int val;
-     ListNode next;
-     ListNode(int x) {
-         val = x;
-         next = null;
-     }
- }
-public  class Ex {
-    public ListNode sortList(ListNode head) {
-        if(head==null || head.next==null){
-            return head;
-        }
-        ListNode mid=getMid(head);
-        ListNode mideNext=mid.next;
-        mid.next=null;
-        return merge(sortList(head),sortList(mideNext));
-    }
-    private ListNode merge(ListNode l1,ListNode l2){
-        if(l1==null){
-            return l2;
-        }
-        if(l2==null){
-            return l1;
-        }
-        ListNode head=new ListNode(0);
-        ListNode cur=head;
-       if(l1.val<l2.val){
-           cur=l1;
-           l1=l1.next;
-       }else {
-           cur=l2;
-           l2=l2.next;
-       }
-        while(l1!=null || l2!=null){
-            if(l1==null){
-                cur.next=l2;
-                return head;
-            }else if(l2==null){
-                cur.next=l1;
-                return head;
-            }else if(l1.val<l2.val){
-                cur.next=l1;
-                l1=l1.next;
-                cur=cur.next;
-            }else{
-                cur.next=l2;
-                l2=l2.next;
-                cur=cur.next;
-            }
 
+import java.util.HashMap;
+import java.util.Map;
+
+public  class Ex {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if(head==null){
+            return null;
         }
-        return head;
-    }
-    private ListNode getMid(ListNode head){
-        if(head==null || head.next==null){
-            return head;
+        Map<RandomListNode,RandomListNode> map=new HashMap<>();
+        RandomListNode colne=head;
+        RandomListNode n=colne;
+        while(n!=null){
+            RandomListNode c=new RandomListNode(n.label);
+            map.put(n,c);
+            n=n.next;
         }
-        ListNode cur=head;
-        while(cur.next!=null && cur.next.next!=null ){
-            head=head.next;
-            cur=cur.next.next;
+        n=colne;
+        while(n!=null){
+            map.get(n).next=map.get(n.next);
+            map.get(n).random=map.get(n.random);
+            n=n.next;
         }
-        return head;
+        return colne;
     }
 }
---
+++ src/Solution007/RandomListNode.java
@@ -0,0 +1,10 @@
+package Solution007;
+
+public class RandomListNode {
+    int label;
+    RandomListNode next;
+    RandomListNode random;
+    public RandomListNode(int lable){
+        this.label=lable;
+    }
+}
