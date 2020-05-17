49d1b492906627c98b60daf9d9d70ddc205cd39c
Nabandon <1839304120@qq.com>
2020-05-17 17:33:07 +0800

    Thread

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -1,8 +1,33 @@
 package Solution007;
 
 public class Main {
-    public static void main(String[] args) {
-        
+    static class MyThread extends Thread{
+        @Override
+        public void run(){
+            System.out.println("thread");
+        }
     }
+    public static void main(String[] args) throws InterruptedException {
+        MyThread myThread=new MyThread();
+        myThread.start();
+        Thread[] threads = new Thread[20];
+        for (int i = 0; i < 20; i++) {
+            int j=i;
+           Thread t= new Thread(new Runnable() {
 
+                @Override
+                public void run() {
+
+                    System.out.println(j);
+                }
+            },"one");
+                t.start();
+                threads[i]=t;
+
+        }
+        for(int i=0;i<20;i++){
+            threads[i].join();
+        }
+        System.out.println("m");
+    }
 }
