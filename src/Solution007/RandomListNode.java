51cf70719da53d3e176d80e49726c7d67075e881
Nabandon <1839304120@qq.com>
2020-05-18 18:13:01 +0800

    gas-station

--- src/Solution007/RandomListNode.java
+++ src/Solution007/RandomListNode.java
@@ -1,10 +1,24 @@
 package Solution007;
 
 public class RandomListNode {
-    int label;
-    RandomListNode next;
-    RandomListNode random;
-    public RandomListNode(int lable){
-        this.label=lable;
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int len=gas.length;
+        if(len==0){
+            return -1;
+        }
+
+        int start=len-1;
+        int end=0;
+        int res=gas[start]-cost[start] ;
+        while(start>end){
+            if(res>=0){
+                res+=gas[end]-cost[end];
+                end++;
+            }else{
+                start--;
+                res+=gas[start]-cost[start];
+            }
+        }
+        return res>=0?start:-1;
     }
 }
