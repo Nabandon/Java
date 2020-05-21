68685c6e1a629b02799e769e7511632428823fb8
Nabandon <1839304120@qq.com>
2020-05-21 12:27:57 +0800

    valid-palindrome

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -1,33 +1,35 @@
 package Solution007;
 
 public class Main {
-    static class MyThread extends Thread{
-        @Override
-        public void run(){
-            System.out.println("thread");
+    public boolean isPalindrome (String s) {
+        // write code here
+        int len=s.length();
+        if(s==null || len<=1) {
+            return true;
         }
+        int left=0;
+        int right=len-1;
+        while(left<right){
+            while(left<right && !is(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !is(s.charAt(right))){
+                right--;
+            }
+            if(Character.toUpperCase(s.charAt(left))!=(Character.toUpperCase(s.charAt(right)))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
-    public static void main(String[] args) throws InterruptedException {
-        MyThread myThread=new MyThread();
-        myThread.start();
-        Thread[] threads = new Thread[20];
-        for (int i = 0; i < 20; i++) {
-            int j=i;
-           Thread t= new Thread(new Runnable() {
-
-                @Override
-                public void run() {
-
-                    System.out.println(j);
-                }
-            },"one");
-                t.start();
-                threads[i]=t;
-
+    private boolean is(char c){
+        if(c<='9'&&c>='0' || c<='z'&&c>='a' || c<='Z'&&c>='A'){
+            return true;
         }
-        for(int i=0;i<20;i++){
-            threads[i].join();
-        }
-        System.out.println("m");
+        return false;
     }
-}
+    }
+
+
