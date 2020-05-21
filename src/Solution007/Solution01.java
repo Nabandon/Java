e44dfb44f0e6e353c46b26ebd7e7aab4a23ad99f
Nabandon <1839304120@qq.com>
2020-05-21 17:01:22 +0800

    best-time-to-buy-and-sell-stock-iii

--- src/Solution007/Solution01.java
+++ src/Solution007/Solution01.java
@@ -5,26 +5,28 @@ import java.util.Stack;
 
 public class Solution01 {
 
-    public int candy(int[] ratings) {
-        int n=ratings.length;
-        if(n==0 || ratings==null){
+    public int maxProfit (int[] prices) {
+        // write code here
+        int len=prices.length;
+        if(len<2){
             return 0;
         }
-        int[] t=new int[n];
-        Arrays.fill(t,1);
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                t[i]=t[i-1]+1;
-            }
+        int[] pre=new int[len];
+        int[] post=new int[len];
+        int min=prices[0];
+        for(int i=1;i<len;i++){
+            min=Math.min(min,prices[i]);
+            pre[i]=Math.max(pre[i-1],prices[i]-min);
         }
-        int sum=0;
-        for(int i=n-1;i>0;i--){
-            sum+=t[i];
-            if(ratings[i]<ratings[i-1] && t[i]>=t[i-1]){
-                t[i-1]=t[i]+1;
-            }
+        int max=prices[len-1];
+        for(int i=len-2;i>=0;i--){
+            max=Math.max(max,prices[i]);
+            post[i]=Math.max(max-prices[i],post[i+1]);
         }
-        sum+=t[0];
-        return sum;
+        int res=0;
+        for(int i=0;i<len;i++){
+            res=Math.max(res,post[i]+pre[i]);
+        }
+        return res;
     }
 }
