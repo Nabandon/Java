0b12e78a70c0f9e4c492227f8fedba15e1b483ac
Nabandon <1839304120@qq.com>
2020-05-22 17:19:57 +0800

    triangle

--- src/Solution007/Solution01.java
+++ src/Solution007/Solution01.java
@@ -1,32 +1,24 @@
 package Solution007;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
 public class Solution01 {
 
-    public int maxProfit (int[] prices) {
-        // write code here
-        int len=prices.length;
-        if(len<2){
+    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+        if(triangle.size()<1){
             return 0;
         }
-        int[] pre=new int[len];
-        int[] post=new int[len];
-        int min=prices[0];
-        for(int i=1;i<len;i++){
-            min=Math.min(min,prices[i]);
-            pre[i]=Math.max(pre[i-1],prices[i]-min);
+        for(int i=triangle.size()-1;i>0;i--){
+            ArrayList<Integer> list=triangle.get(i-1);
+            ArrayList<Integer> list2=triangle.get(i);
+            for(int j=0;j<list.size();j++){
+                int t=list.get(j);
+                int t2=Math.min(list2.get(j),list2.get(j+1));
+                list.set(j,t+t2);
+            }
         }
-        int max=prices[len-1];
-        for(int i=len-2;i>=0;i--){
-            max=Math.max(max,prices[i]);
-            post[i]=Math.max(max-prices[i],post[i+1]);
-        }
-        int res=0;
-        for(int i=0;i<len;i++){
-            res=Math.max(res,post[i]+pre[i]);
-        }
-        return res;
+        return triangle.get(0).get(0);
     }
 }
