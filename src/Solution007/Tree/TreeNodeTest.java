7271a69722f0d5c5c993d2869d9610ff7c8a0914
Nabandon <1839304120@qq.com>
2020-05-25 16:29:20 +0800

    convert-sorted-list-to-binary-search-tree

--- src/Solution007/Tree/TreeNodeTest.java
+++ src/Solution007/Tree/TreeNodeTest.java
@@ -1,26 +1,43 @@
 package Solution007.Tree;
 
 import java.util.ArrayList;
+import java.util.List;
 
-class TreeNode{
+class ListNode {
     int val;
-    TreeNode right;
-    TreeNode left;
-    TreeNode(int val){
-        this.val=val;
-    }
+    ListNode next;
 }
-public class TreeNodeTest {
-    public boolean hasPathSum (TreeNode root, int sum) {
+    class TreeNode {
+        int val;
+        TreeNode right;
+        TreeNode left;
 
-        if(root==null){
-            return false;
+
+         TreeNode(int val) {
+            this.val=val;
         }
-        if(root.left==null && root.right==null && root.val==sum){
-            return true;
-        }
-        boolean l= hasPathSum(root.left,sum-root.val);
-        boolean r= hasPathSum(root.right,sum-root.val);
-        return l||r;
     }
+
+    public class TreeNodeTest {
+        public TreeNode sortedListToBST(ListNode head) {
+            // write code here
+
+            if (head == null) {
+                return null;
+            }
+            if(head.next==null){
+                return new TreeNode(head.val);
+            }
+            ListNode fast=head.next;
+            ListNode slow=head;
+            while(fast.next!=null && fast.next.next!=null){
+                fast=fast.next.next;
+                slow=slow.next;
+            }
+            TreeNode root=new TreeNode(slow.next.val);
+            root.right=sortedListToBST(slow.next.next);
+            slow.next=null;
+            root.left=(sortedListToBST(head));
+            return root;
+        }
 }
