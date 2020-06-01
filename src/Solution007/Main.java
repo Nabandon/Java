748f1e172679dc66c61c047e35cda9d7b2ccf0a4
Nabandon <1839304120@qq.com>
2020-06-01 11:34:21 +0800

    StatisticsPalindromeString

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -1,22 +1,42 @@
 package Solution007;
 
+import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public boolean isPalindrome (String s) {
-        // write code here
-        int len=s.length();
-        if(s==null || len<=1) {
-            return true;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        int num=help(A,B);
+        System.out.println(num);
+    }
+    private static int help(String a,String b){
+        ArrayList<Character> list=new ArrayList<>();
+        int lena=a.length();
+        int lenb=b.length();
+        int num=0;
+        for(int i=0;i<=lena;i++){
+            list.clear();
+            for(int k=0;k<lena;k++){
+                list.add(a.charAt(k));
+            }
+            for(int j=b.length()-1;j>=0;j--){
+                list.add(i,b.charAt(j));
+            }
+            if(palindrome(list,lena+lenb)){
+                num++;
+            }
         }
+        return num;
+    }
+    private static boolean palindrome(ArrayList list,int len){
         int left=0;
         int right=len-1;
-        while(left<right){
-            while(left<right && !is(s.charAt(left))){
-                left++;
-            }
-            while(left<right && !is(s.charAt(right))){
-                right--;
-            }
-            if(Character.toUpperCase(s.charAt(left))!=(Character.toUpperCase(s.charAt(right)))){
+        while(left<=right){
+            if(!list.get(left).equals(list.get(right))){
                 return false;
             }
             left++;
@@ -24,12 +44,6 @@ public class Main {
         }
         return true;
     }
-    private boolean is(char c){
-        if(c<='9'&&c>='0' || c<='z'&&c>='a' || c<='Z'&&c>='A'){
-            return true;
-        }
-        return false;
-    }
-    }
+}
 
 
