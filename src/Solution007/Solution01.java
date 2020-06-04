66c6c28720466e00eac8d0e850ba9515ad25942a
Nabandon <1839304120@qq.com>
2020-06-04 14:01:09 +0800

    anagrams

--- src/Solution007/Solution01.java
+++ src/Solution007/Solution01.java
@@ -1,24 +1,31 @@
 package Solution007;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 public class Solution01 {
 
-    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-        if(triangle.size()<1){
-            return 0;
-        }
-        for(int i=triangle.size()-1;i>0;i--){
-            ArrayList<Integer> list=triangle.get(i-1);
-            ArrayList<Integer> list2=triangle.get(i);
-            for(int j=0;j<list.size();j++){
-                int t=list.get(j);
-                int t2=Math.min(list2.get(j),list2.get(j+1));
-                list.set(j,t+t2);
+    public ArrayList<String> anagrams(String[] strs) {
+        ArrayList<String> list=new ArrayList<>();
+        HashMap<String,Integer> map=new HashMap<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<strs.length;i++){
+            char[] c=strs[i].toCharArray();
+            Arrays.sort(c);
+            String s=new String(c);
+            if(!map.containsKey(s)){
+                map.put(s,i);
+            }else{
+                set.add(map.get(s));
+                set.add(i);
+
             }
         }
-        return triangle.get(0).get(0);
+        Iterator it=set.iterator();
+        while(it.hasNext()){
+            int n=(int)it.next();
+            list.add(strs[n]);
+
+        }
+        return list;
     }
 }
