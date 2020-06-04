4b9c8e00643a9492d336b8fd530091437f4fd1b1
Nabandon <1839304120@qq.com>
2020-06-04 10:30:52 +0800

    删除公共字符串

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -2,39 +2,30 @@ package Solution007;
 
 import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String str=sc.next();
-
-       StringBuilder s2=new StringBuilder();
-        int i=0;
-        int index=0;
-        while(index<str.length()){
-            int k=0;
-            StringBuilder s1=new StringBuilder();
-            if(index<str.length()&&str.charAt(index)<='9' && str.charAt(index)>='0') {
-                while (index<str.length()&&str.charAt(index) <= '9' && str.charAt(index) >= '0') {
-                    k++;
-                    s1.append(str.charAt(index));
-                    index++;
+        Scanner sca=new Scanner(System.in);
+        while(sca.hasNext()){
+            String s1=sca.nextLine();
+            String s2=sca.nextLine();
+            StringBuilder str=new StringBuilder();
+            for(int i=0;i<s1.length();i++){
+                if(!s2.contains(s1.charAt(i)+"")){
+                    str.append(s1.charAt(i));
                 }
-            }
-            if(k>i){
-                i=k;
-               s2=new StringBuilder();
-               s2.append(s1);
 
             }
-
-            index++;
+            System.out.println(str.toString());
         }
-
-        System.out.println(s2.toString());
     }
-}
+    }
+
+
+
 
 
