ca201036c78cec0c52be4b81bc2026fa68f52651
Nabandon <1839304120@qq.com>
2020-06-06 09:13:53 +0800

    meiriyiti

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -9,23 +9,52 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        Scanner sca=new Scanner(System.in);
+        int[] arr=new int[4];
+        String str=sca.nextLine();
+        String[] s=str.split(" ");
+        for(int i=0;i<4;i++){
+            arr[i]=Integer.parseInt(s[i]);
         }
-        System.out.println(sum(0,0,arr));
+        if((arr[0]+arr[2])%2!=0){
+            System.out.println("No");
+            return ;
+        }
+        int a=(arr[0]+arr[2])/2;
+        if(arr[2]-a<0){
+            System.out.println("No");
+            return ;
+        }
+        int b=arr[2]-a;
+        if(b-arr[1]<0){
+            System.out.println("No");
+            return ;
+        }
+        int c=b-arr[1];
+        if((b+c)!=arr[3]){
+            System.out.println("No");
+            return ;
+        }
+        System.out.println(a+" "+b+" "+c);
     }
-    private static int sum(int i,int res,int[] arr){
-        if(res==40){
-            return 1;
-        }
-        if(i==arr.length || res>40 ){
-            return 0;
-        }
-        return sum(i+1,res,arr)+sum(i+1,res+arr[i],arr);
-    }
+//    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int[] arr=new int[n];
+//        for(int i=0;i<n;i++){
+//            arr[i]=sc.nextInt();
+//        }
+//        System.out.println(sum(0,0,arr));
+//    }
+//    private static int sum(int i,int res,int[] arr){
+//        if(res==40){
+//            return 1;
+//        }
+//        if(i==arr.length || res>40 ){
+//            return 0;
+//        }
+//        return sum(i+1,res,arr)+sum(i+1,res+arr[i],arr);
+//    }
 //    public static void main(String[] args){
 //        Scanner sca=new Scanner(System.in);
 //        while(sca.hasNext()){
