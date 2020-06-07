0257126f80c4bcc4ed0b22ef47716b9d065230e6
Nabandon <1839304120@qq.com>
2020-06-07 11:48:43 +0800

    two_sum

--- src/Solution007/Ex.java
+++ src/Solution007/Ex.java
@@ -1,28 +1,37 @@
 package Solution007;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
-import java.util.*;
+public class Ex {
 
-public  class Ex {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        while(s.hasNext()){
-            int n=s.nextInt();
-            int res=0;
-            while(n>0){
-                if(n%8==0){
-                    System.out.println(res+n/8);
-                }else {
-                    res++;
-                    n-=6;
-                }
-            }
-            if(n==0){
-                System.out.println(res);
-            }else {
-                System.out.println(-1);
+static int i=0;
+    public int[] twoSum (int[] numbers, int target) {
+//        // write code here
+//        for(int i=0;i<numbers.length;i++){
+//            for(int j=i+1;j<numbers.length;j++){
+//                if(numbers[i]+numbers[j]==target){
+//                    return new int[]{i+1,j+1};
+//                }
+//            }
+//        }
+//        return null;
+
+       // 哈希
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] res=new int[2];
+        for(int i=0;i<numbers.length;i++){
+            if(map.containsKey(numbers[i])){
+                res[0]=map.get(numbers[i])+1;
+                res[1]=i+1;
+                break;
+            }else{
+                map.put(target-numbers[i],i);
             }
         }
+        return res;
     }
 }
