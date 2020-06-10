d4e864637d907b5239fc418e202c6225540efd9c
Nabandon <1839304120@qq.com>
2020-06-10 10:58:30 +0800

    数字颠倒

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -9,26 +9,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            int n = sc.nextInt();
-            if (n > 1000) {
-                n = 999;
-            }
-            List<Integer> list = new ArrayList<Integer>();
-            for (int i = 0; i < n; i++) {
-                list.add(i);
-            }
-            int i = 0;
-            while (list.size() > 1) {
-                i = (i + 2) % list.size();
-                list.remove(i);
-            }
-            System.out.println(list.get(0));
+    public static void main(String[] args){
+        Scanner sca=new Scanner(System.in);
+        int n=sca.nextInt();
+        String str="";
+        while(n/10!=0){
+            int w=n%10;
+            str+=w;
+            n/=10;
         }
+        str+=n;
+        System.out.println(str);
     }
-
 //    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 //        if (nums1.length > nums2.length) {
 //            return findMedianSortedArrays(nums2, nums1);
