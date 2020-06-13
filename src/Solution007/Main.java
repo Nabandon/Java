3b9a3bfc3fcd254a00382880fd747aca2f5f42fe
Nabandon <1839304120@qq.com>
2020-06-13 09:59:13 +0800

    格雷码

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -4,23 +4,21 @@ package Solution007;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sca=new Scanner(System.in);
-        while(sca.hasNext()){
-            String s=sca.nextLine();
-            int[] arr=new int[10];
-            for(int i=0;i<s.length();i++){
-
-                arr[Integer.parseInt(s.charAt(i)+"")]++;
-            }
-            for(int j=0;j<arr.length;j++){
-                if(arr[j]!=0){
-                    System.out.println(j+":"+arr[j]);
-                }
+    public String[] getGray(int n) {
+        // write code here
+        String[] str=null;
+        if(n==1){
+            str= new String[]{"0","1"};
+        }else{
+            String[] res=getGray(n-1);
+            str=new String[res.length*2];
+            for(int i=0;i<res.length;i++){
+                str[i]="0"+res[i];
+                str[str.length-1-i]="1"+res[i];
             }
 
         }
-
+        return str;
     }
 
 }
