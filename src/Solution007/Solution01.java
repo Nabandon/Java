2f9f6091ed0cba0a1579cc89795315692b504f8d
Nabandon <1839304120@qq.com>
2020-06-16 10:02:43 +0800

    zuMin

--- src/Solution007/Solution01.java
+++ src/Solution007/Solution01.java
@@ -3,29 +3,25 @@ package Solution007;
 import java.util.*;
 
 public class Solution01 {
+    public static void main(String[] args){
+        Scanner sca=new Scanner(System.in);
+        while(sca.hasNext()){
+            String[] str=sca.nextLine().split(" ");
+            int i0=Integer.parseInt(str[0]);
+            StringBuffer res=new StringBuffer();
 
-    public ArrayList<String> anagrams(String[] strs) {
-        ArrayList<String> list=new ArrayList<>();
-        HashMap<String,Integer> map=new HashMap<>();
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<strs.length;i++){
-            char[] c=strs[i].toCharArray();
-            Arrays.sort(c);
-            String s=new String(c);
-            if(!map.containsKey(s)){
-                map.put(s,i);
-            }else{
-                set.add(map.get(s));
-                set.add(i);
-
+            for(int i=1;i<str.length;i++){
+                int n=Integer.parseInt(str[i]);
+                for(int j=0;j<n;j++){
+                    res.append(i);
+                }
             }
+            if(i0!=0){
+                for(int i=0;i<i0;i++){
+                    res.insert(1,0);
+                }
+            }
+            System.out.println(res.toString());
         }
-        Iterator it=set.iterator();
-        while(it.hasNext()){
-            int n=(int)it.next();
-            list.add(strs[n]);
-
-        }
-        return list;
     }
 }
