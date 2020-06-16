428a4278877bebb94ee70e12e54db10def90bb3a
Nabandon <1839304120@qq.com>
2020-06-16 12:17:32 +0800

    尼科彻斯

--- src/Solution007/Solution01.java
+++ src/Solution007/Solution01.java
@@ -6,22 +6,20 @@ public class Solution01 {
     public static void main(String[] args){
         Scanner sca=new Scanner(System.in);
         while(sca.hasNext()){
-            String[] str=sca.nextLine().split(" ");
-            int i0=Integer.parseInt(str[0]);
-            StringBuffer res=new StringBuffer();
-
-            for(int i=1;i<str.length;i++){
-                int n=Integer.parseInt(str[i]);
-                for(int j=0;j<n;j++){
-                    res.append(i);
+            int n=sca.nextInt();
+            StringBuilder str=new StringBuilder();
+            int lf=n*n*n;
+            int a1=(lf-n*(n-1))/n;
+            for(int i=0;i<n;i++){
+                if(i==n-1){
+                    str.append(a1);
+                    System.out.println(str.toString());
+                    return;
+                }else{
+                    str.append(a1).append("+");
+                    a1+=2;
                 }
             }
-            if(i0!=0){
-                for(int i=0;i<i0;i++){
-                    res.insert(1,0);
-                }
-            }
-            System.out.println(res.toString());
         }
     }
 }
