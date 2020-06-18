60c53b6a40057c5222e2ab122e6583547e3d2a28
Nabandon <1839304120@qq.com>
2020-06-18 12:06:56 +0800

    xp

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -1,29 +1,55 @@
 package Solution007;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args){
-        Scanner sca=new Scanner(System.in);
-        int lo=sca.nextInt();
-        int count=0;
-
-        while(count<=300000){
-            if(lo%1000000007==0){
-                break;
-            }else{
-                count++;
-                lo=(2*lo+1)%1000000007;
-            }
+    public static void main(String[] args) {
+        Scanner sca = new Scanner(System.in);
+        int count = sca.nextInt();
+        if(count<=0){
+            System.out.println((char[]) null);
+            return;
         }
-        int res = (count + 2) / 3;
-        System.out.println(res > 100000 ? -1 : res);
+        for (int i = 0; i < count; i++) {
+            int n = sca.nextInt();
+            int k = sca.nextInt();
+
+
+            int[] arr = new int[2*n];
+            for(int j=0;j<2*n;j++){
+                arr[j]=sca.nextInt();
+            }
+            for (int m = 0; m < k; m++) {
+                x(arr);
+            }
+            for(int c=0;c<arr.length;c++){
+                if(c==arr.length-1){
+                    System.out.print(arr[c]);
+                }else{
+                    System.out.print(arr[c]+" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static void x(int[] arr) {
+        int[] a=new int[arr.length];
+        int j=0;
+        for(int i=0;i<arr.length;i+=2){
+            a[i]=arr[j++];
+        }
+        for(int i=1;i<arr.length;i+=2){
+            a[i]=arr[j++];
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=a[i];
+        }
     }
 }
-
-
 
 
 
