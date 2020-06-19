643ae581ac9b55e74abfcbc0a58c874652a4325d
Nabandon <1839304120@qq.com>
2020-06-19 12:17:16 +0800

    stack

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -1,26 +1,43 @@
 package Solution007;
 
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    static ArrayList<String> list=new ArrayList<>();
     public static void main(String[] args){
         Scanner sca=new Scanner(System.in);
         while(sca.hasNext()){
+            list.clear();
             int n=sca.nextInt();
-            if(n==0){
-                return;
-            }
-
-            int f=sca.nextInt();
-            int count=0;
+            int[] arr=new int[n];
             for(int i=0;i<n;i++){
-                if(f==sca.nextInt()){
-                    count++;
-                }
+                arr[i]=sca.nextInt();
             }
-            System.out.println(count);
+            Stack<Integer> stack=new Stack<>();
+            h(arr,stack,"",0,0);
+            Collections.sort(list);
+            for(String s:list){
+                System.out.println(s);
+            }
+        }
+        sca.close();
+    }
+    private static void h(int[] arr,Stack stack,String str,int j,int c){
+        if(c==arr.length){
+            list.add(str);
+            return;
+        }
+        if(c<arr.length && !stack.isEmpty()){
+            int t=(int)stack.pop();
+            h(arr,stack,str+t+" ",j,c+1);
+            stack.push(t);
+        }
+        if(j<arr.length){
+            stack.push(arr[j]);
+            h(arr,stack,str,j+1,c);
+            stack.pop();
         }
     }
 }
