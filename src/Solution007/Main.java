2617954004a9072dd2a1648f839891a74778a3be
Nabandon <1839304120@qq.com>
2020-06-21 10:36:50 +0800

    序列

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -4,39 +4,34 @@ package Solution007;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
-        int[] arr = new int[count];
-        for(int i = 0;i < count;i++){
-            arr[i] = sc.nextInt();
-        }
-        int[][] res=getIndex(arr);
-        for(int i=0;i<arr.length;i++){
-            System.out.println(res[i][0]+" "+res[i][1]);
-        }
-    }
-    private static int[][] getIndex(int[] arr){
-        Stack<Integer> stack=new Stack<>();
-        int[][] in=new int[arr.length][2];
-        for(int i=0;i<arr.length;i++){
-            while(!stack.isEmpty() && arr[stack.peek()]>arr[i]){
-                int now=stack.pop();
-                int left=stack.isEmpty()?-1:stack.peek();
-                in[now][0]=left;
-                in[now][1]=i;
+
+        public static void main(String[] args){
+            Scanner sca=new Scanner(System.in);
+            while(sca.hasNext()){
+                String str=sca.next();
+                int len=sca.nextInt();
+                if(str.length()<len){
+                    return;
+                }
+                int index=0;
+                int max=0;
+                for(int i=0;i<str.length()-len;i++){
+                    int maxt=0;
+                    for(int j=i;j<i+len;j++){
+                        if(str.charAt(j)=='G' || str.charAt(j)=='C'){
+                            maxt++;
+                        }
+                    }
+                    if(maxt>max){
+                        max=maxt;
+                        index=i;
+                    }
+                }
+                System.out.println(str.substring(index,index+len));
             }
-            stack.push(i);
         }
-        while(!stack.isEmpty()){
-            int now=stack.pop();
-            int left=stack.isEmpty()?-1:stack.peek();
-            in[now][0]=left;
-            in[now][1]=-1;
-        }
-        return in;
     }
-}
+
 
 
 
