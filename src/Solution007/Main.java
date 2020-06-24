33b81b0dfcec526d23d7dd05e920fd785adb3aca
Nabandon <1839304120@qq.com>
2020-06-24 12:57:56 +0800

    code

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -4,73 +4,28 @@ package Solution007;
 import java.util.*;
 
 public class Main {
-
-//    public boolean[] chkSubStr(String[] p, int n, String s) {
-//        // write code here
-//        boolean[] b=new boolean[n];
-//        if(n<=0 || s.length()<=0 ||s==null){
-//            return b;
-//        }
-//        for(int i=0;i<n;i++){
-//            boolean res=s.contains(p[i]);
-//            b[i]=res;
-//        }
-//        return b;
-//    }
-public static void main(String[] args){
-    Scanner sca=new Scanner(System.in);
-    while(sca.hasNext()){
-        int n=sca.nextInt();
-        int des=sca.nextInt();
-        int[] arr=new int[n];
-        String[] str=new String[n];
-        for(int i=0;i<n;i++){
-            str[i]=sca.next();
-            arr[i]=sca.nextInt();
-        }
-        if(des==0){
-            for(int j=0;j<n-1;j++){
-                String st=str[j+1];
-                int t=arr[j+1];
-                int k=0;
-                for(k=j+1;k>0;k--){
-                    if(t>arr[k-1]){
-                        arr[k]=arr[k-1];
-                        str[k]=str[k-1];
+    public static void main(String[] args){
+        Scanner sca=new Scanner(System.in);
+        while(sca.hasNext()){
+            String str=sca.nextLine();
+            StringBuffer res=new StringBuffer();
+            for(int i=0;i<str.length();i++){
+                char c=str.charAt(i);
+                if(c!=' '){
+                    int t=(c+22-'A')%26 +'A'-1 ;
+                    if(t<'A'){
+                        res.append("Z");
                     }else{
-                        arr[k]=t;
-                        str[k]=st;
-                        break;
+                        res.append((char)t);
                     }
+                }else{
+                    res.append(c);
                 }
-                arr[k]=t;
-                str[k]=st;
             }
-        }else{
-            for(int j=0;j<n-1;j++){
-                String st=str[j+1];
-                int t=arr[j+1];
-                int k=0;
-                for(k=j+1;k>0;k--){
-                    if(t<arr[k-1]){
-                        arr[k]=arr[k-1];
-                        str[k]=str[k-1];
-                    }else{
-                        arr[k]=t;
-                        str[k]=st;
-                        break;
-                    }
-                }
-                arr[k]=t;
-                str[k]=st;
-            }
-        }
-        for(int i=0;i<n;i++){
-            System.out.println(str[i]+" "+arr[i]);
+            System.out.println(res.toString());
         }
     }
 }
-    }
 
 
 
