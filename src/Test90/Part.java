26ef6490bf2d75eb0b843844b5cf74db0ee5f4d1
Nabandon <1839304120@qq.com>
2020-06-25 13:29:24 +0800

    链式A+B

--- src/Test90/Part.java
+++ src/Test90/Part.java
@@ -3,17 +3,57 @@ package Test90;
 import java.util.Stack;
 
 public class Part {
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
-        if(pushed.length<=2) return true;
-        int j=0;
-        Stack<Integer> stack=new Stack<>();
-        for(int x:pushed){
-            stack.push(x);
-            while(!stack.isEmpty() && j<popped.length && stack.peek()==popped[j]){
-                stack.pop();
-                j++;
+    public ListNode plusAB(ListNode a, ListNode b) {
+        // write code here
+        ListNode res=new ListNode(0);
+        ListNode pre=res;
+        int n=0;
+        while(a!=null && b!=null){
+            int  sum=a.val+b.val+n;
+            if(sum>9){
+                n=1;
+                sum-=10;
+                pre.next=new ListNode(sum);
+            }else{
+                n=0;
+                pre.next=new ListNode(sum);
             }
+            pre=pre.next;
+            a=a.next;
+            b=b.next;
         }
-        return j==popped.length;
+
+        while(a!=null){
+            int  sum=a.val+n;
+            if(sum>9){
+                n=1;
+                sum-=10;
+
+            }else{
+                n=0;
+            }
+            pre.next=new ListNode(sum);
+            pre=pre.next;
+            a=a.next;
+        }
+        while(b!=null){
+            int  sum=b.val+n;
+            if(sum>9){
+                n=1;
+                sum-=10;
+
+            }else{
+                n=0;
+            }
+            pre.next=new ListNode(sum);
+            pre=pre.next;
+            b=b.next;
+        }
+        if(n==1){
+            pre.next=new ListNode(1);
+            return res.next;
+        }else{
+            return res.next;
+        }
     }
 }
