1cdc19dc513795b94c9af5f821bf3e044f5c1508
Nabandon <1839304120@qq.com>
2020-06-27 17:12:53 +0800

    test

--- src/Test90/Arr.java
+++ src/Test90/Arr.java
@@ -1,69 +1,33 @@
 package Test90;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Arr {
     public static void main(String[] args){
         Scanner sca=new Scanner(System.in);
-        int n=sca.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sca.nextInt();
-        }
-        int a1=0,a2=0,a3=0,a5=0,c=0 ,a4c=0;
-        double a4=0.0;
-        boolean t=false;
-        for(int i=0;i<n;i++){
-            if(arr[i]%10==0){
-                a1+=arr[i];
-            }
-            if(arr[i]%5==1){
-                t=true;
-                if(c==0){
-                    a2+=arr[i];
-                    c=1;
+        while(sca.hasNext()){
+            int n=sca.nextInt();
+            int a=sca.nextInt();
+            for(int i=0;i<n;i++){
+                int b=sca.nextInt();
+                if(b<=a){
+                    a+=b;
                 }else{
-                    a2-=arr[i];
-                    c=0;
+                    a+=g(a,b);
                 }
             }
-            if(arr[i]%5==2){
-                a3++;
-            }
-            if(arr[i]%5==3){
-                a4c++;
-                a4+=arr[i];
-            }
-            if(arr[i]%5==4 && arr[i]>a5){
-                a5=arr[i];
-            }
+            System.out.println(a);
         }
-        if(a1!=0){
-            System.out.print(a1+" ");
-        }else{
-            System.out.print("N ");
+    }
+    private static int g(int a,int b){
+        while(a>0){
+            int t=a;
+            a=b%a;
+            b=t;
         }
-        if(t){
-            System.out.print(a2+" ");
-        }else{
-            System.out.print("N ");
-        }
-        if(a3!=0){
-            System.out.print(a3+" ");
-        }else{
-            System.out.print("N ");
-        }
-        if(a4!=0){
-            double m=a4/a4c;
-            System.out.printf("%.1f",m);
-            System.out.print(" ");
-        }else{
-            System.out.print("N ");
-        }
-        if(a5!=0){
-            System.out.print(a5+" ");
-        }else{
-            System.out.print("N ");
-        }
+        return b;
     }
 }
