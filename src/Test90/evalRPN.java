6976a6ef01512f713d78ddc143a29b5bb060a7dc
Nabandon <1839304120@qq.com>
2020-06-27 17:48:07 +0800

    Test90/evalRPN.java

--- src/Test90/evalRPN.java
+++ src/Test90/evalRPN.java
@@ -1,28 +1,37 @@
 package Test90;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class evalRPN {
-    public int evalRPN(String[] tokens) {
-        Stack<Integer> stack=new Stack<>();
-        for(String s:tokens){
-            if(s.equals("+")){
-                stack.push(stack.pop()+stack.pop());
-            }
-            if(s.equals("-")){
-                stack.push(-stack.pop()+stack.pop());
-            }
-            if(s.equals("*")){
-                stack.push(stack.pop()*stack.pop());
-            }
-            if(s.equals("/")){
-                int num=stack.pop();
-                stack.push(stack.pop()/num);
-            }else{
+    public static void main(String[] args){
+        Scanner sca=new Scanner(System.in);
+       // while(sca.hasNext()){
+            int n=sca.nextInt();
+            int m=sca.nextInt();
+            int[] nn=new int[n+1];
 
-                stack.push(Integer.parseInt(s));
+            for(int i=1;i<n+1;i++){
+                nn[i]=sca.nextInt();
+            }
+            for(int i=0;i<m;i++){
+                String c=sca.next();
+                int a=sca.nextInt();
+                int b=sca.nextInt();
+                if(c.equals("U")){
+                   nn[a]=b;
+                }else {
+                    int at=Math.min(a,b);
+                    int bt=Math.max(a,b);
+                    int t=nn[at];
+                    for(int j=at;j<=bt;j++){
+                        if(nn[j]>t){
+                            t=nn[j];
+                        }
+                    }
+                    System.out.println(t);
+                }
             }
         }
-        return stack.pop();
-    }
+    //}
 }
