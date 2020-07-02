e1dd8b845a6588ede3cf60b9704912ba07005048
Nabandon <1839304120@qq.com>
2020-07-02 11:00:13 +0800

    守型

--- src/Test90/Arr.java
+++ src/Test90/Arr.java
@@ -1,33 +1,33 @@
 package Test90;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Arr {
+public class Arr{
     public static void main(String[] args){
         Scanner sca=new Scanner(System.in);
         while(sca.hasNext()){
             int n=sca.nextInt();
-            int a=sca.nextInt();
-            for(int i=0;i<n;i++){
-                int b=sca.nextInt();
-                if(b<=a){
-                    a+=b;
-                }else{
-                    a+=g(a,b);
-                }
+            int    po=(int)Math.pow(n,2);
+            int m=0;
+            int pf=po;
+            while(pf/10>0){
+                m++;
+                pf/=10;
             }
-            System.out.println(a);
+            m=po%((int)Math.pow(10,m));
+            if(m==n){
+                System.out.println("Yes!");
+            }else{
+                System.out.println("No!");
+            }
         }
-    }
-    private static int g(int a,int b){
-        while(a>0){
-            int t=a;
-            a=b%a;
-            b=t;
-        }
-        return b;
     }
 }
+
+
