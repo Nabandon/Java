a0a901bdd353686ca50857e978795571af67cdf5
Nabandon <1839304120@qq.com>
2020-07-02 11:56:21 +0800

    mimayanzheng

--- src/Test90/Arr.java
+++ src/Test90/Arr.java
@@ -3,28 +3,45 @@ package Test90;
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.*;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Arr{
     public static void main(String[] args){
         Scanner sca=new Scanner(System.in);
         while(sca.hasNext()){
-            int n=sca.nextInt();
-            int    po=(int)Math.pow(n,2);
-            int m=0;
-            int pf=po;
-            while(pf/10>0){
-                m++;
-                pf/=10;
-            }
-            m=po%((int)Math.pow(10,m));
-            if(m==n){
-                System.out.println("Yes!");
+            String str=sca.next();
+            if(str.length()<=8){//长度
+                System.out.println("NG");
             }else{
-                System.out.println("No!");
+                HashMap<Integer,Boolean> map=new HashMap<>();
+                boolean b=true;
+                for(int i=0;i<str.length();i++){
+                    if(i<str.length()-3){//是否重复
+                        String q=str.substring(i,i+3);
+                        String h=str.substring(i+3);
+                        if(h!=null && h.contains(q)){
+                            b=false;
+                            break;
+                        }
+                    }
+                    char c=str.charAt(i);//种类够否
+                    if(c<='9' && c>='0'){
+                        map.put(1,true);
+                    }else if(c<'z' && c>'a'){
+                        map.put(2,true);
+                    }else if(c<'Z' && c>'A'){
+                        map.put(3,true);
+                    }else{
+                        map.put(4,true);
+                    }
+                }
+                int co=map.size();
+                if(co>=3 && b){
+                    System.out.println("OK");
+                }else{
+                    System.out.println("NG");
+                }
             }
         }
     }
