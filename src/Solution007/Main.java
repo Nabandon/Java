eb992a3f244e6a421589f77152f729d5783859c3
Nabandon <1839304120@qq.com>
2020-07-12 12:24:21 +0800

    按K翻转链表

--- src/Solution007/Main.java
+++ src/Solution007/Main.java
@@ -1,33 +1,46 @@
 package Solution007;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
-import java.util.*;
 
-public class Main {
-    public static void main(String[] args){
-        Scanner sca=new Scanner(System.in);
-        while(sca.hasNext()){
-            String str=sca.nextLine();
-            StringBuffer res=new StringBuffer();
-            for(int i=0;i<str.length();i++){
-                char c=str.charAt(i);
-                if(c!=' '){
-                    int t=(c+22-'A')%26 +'A'-1 ;
-                    if(t<'A'){
-                        res.append("Z");
-                    }else{
-                        res.append((char)t);
-                    }
-                }else{
-                    res.append(c);
-                }
-            }
-            System.out.println(res.toString());
+public class Main{
+    static class ListNode{
+        public int val;
+        public ListNode next;
+        public ListNode(int val){
+            this.val=val;
         }
     }
-}
+    public static void main(String[] args){
+        Scanner sca=new Scanner(System.in);
+        ArrayList<Integer> list=new ArrayList<>();
+        while(sca.hasNext()){
+            list.add(sca.nextInt());
+        }
+        int len=list.size();
+        if(len<1){
+            return;
+        }
+        int k=list.get(len-1);
 
-
-
-
-
+        ListNode head=new ListNode(0);
+        ListNode cur=head;
+        for(int i=k-1;i<len-1;i+=k){
+            for(int j=i;j>i-k;j--){
+                cur.next=new ListNode(list.get(j));
+                cur=cur.next;
+            }
+        }
+        int c=(len-1)/k;
+        int qishi=c*k;
+        for(;qishi<len-1;qishi++){
+            cur.next=new ListNode(list.get(qishi));
+            cur=cur.next;
+        }
+        while(head.next!=null){
+            System.out.print(head.next.val+" ");
+            head=head.next;
+        }
+    }
+} 
