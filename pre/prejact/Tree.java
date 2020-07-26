070ad7883dd26dfde0b1c5d171d369b9df0ef07e
Nabandon <1839304120@qq.com>
2020-07-26 10:50:50 +0800

    二叉树中和为某值的路径

--- pre/prejact/Tree.java
+++ pre/prejact/Tree.java
@@ -1,6 +1,36 @@
 package prejact;
 
+
+import java.util.ArrayList;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+}
 public class Tree {
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        h(root,target,res,list);
+        return res;
+    }
+    private void h(TreeNode root,int target,ArrayList<ArrayList<Integer>> res,ArrayList<Integer> list){
+        if(root==null){
+            return;
+        }
+        list.add(root.val);
+        target-=root.val;
+        if(root.left==null && root.right==null && target==0){
+            res.add(new ArrayList<>(list));
+        }
+        h(root.left,target,res,list);
+        h(root.right,target,res,list);
+        list.remove(list.size()-1);
+    }
     public long tree1 (int[] a) {
         // write code here
         int len=a.length;
